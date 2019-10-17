708322ab740f5bafe1cf1feea8f1a3601e21ed7a
rconn478 <rconn478@gmail.com>
2019-10-17 12:36:50 -0500

    Fixed to comply with pylint suggestions

--- autoscheduler/autoscheduler/config/config.py
+++ autoscheduler/autoscheduler/config/config.py
@@ -1,23 +1,26 @@
+"""
+Read settings to be user by Django from postgres-info.json
+"""
 import os
+import sys
 import json
-
 
 def get_prop(prop):
     """
-    get property from postgres-info.json, or create that file if needed
+    Get property from postgres-info.json, or create that file if needed
     """
-    infoFile = os.path.dirname(__file__) + "/postgres-info.json"
+    info_file = os.path.dirname(__file__) + "/postgres-info.json"
     try:
-        info = json.load(open(infoFile, "r"))
+        info = json.load(open(info_file, "r"))
         read_prop = info.get(prop)
         if prop == "user" and read_prop == "":
-            print(f"Please configure {os.path.abspath(infoFile)} to include "
-            "your postgres user and password.")
-            exit()
+            print(f"Please configure {os.path.abspath(info_file)} to include "
+                  "your postgres user and password.")
+            sys.exit()
         return read_prop
-    except FileNotFoundError as error:
+    except FileNotFoundError:
         user_config = json.loads('{"user": "", "password": ""}')
-        json.dump(user_config, open(infoFile, "w"), indent=4)
-        print(f"Created postgres config file in {os.path.abspath(infoFile)}.\n"
-        "Please configure it with your postgres username and password.")
-        exit()
+        json.dump(user_config, open(info_file, "w"), indent=4)
+        print(f"Created postgres config file in {os.path.abspath(info_file)}.\n"
+              "Please configure it with your postgres username and password.")
+        sys.exit()
