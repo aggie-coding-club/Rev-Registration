1b11cd3386f17ef887d11b5d84eff5af40ba154e
Adel Hassan <sean.dashals@gmail.com>
2019-10-19 23:25:25 -0500

    Fixed linting suggestions

--- autoscheduler/frontend/src/webpack.config.js
+++ autoscheduler/frontend/src/webpack.config.js
@@ -27,11 +27,11 @@ module.exports = {
     ],
   },
 
-  // When importing a module whose path matches one of the following, just assume a corresponding global
-  // variable exists and use that instead. This is important b/c it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds
+  // When importing a module whose path matches one of the following, just assume a corresponding
+  // global variable exists and use that instead. This is important b/c it allows us to avoid
+  // bundling all of our dependencies, which allows browsers to cache those libraries between builds
   externals: {
-    'react': 'React',
+    react: 'React',
     'react-dom': 'ReactDOM',
   },
 };
