6e074f7b88a3881f96162b4a89ad603b1ec07024
Adel Hassan <sean.dashals@gmail.com>
2019-10-24 13:10:16 -0500

    Added routing

--- autoscheduler/frontend/src/App.tsx
+++ autoscheduler/frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { Router, Link } from '@reach/router';
 
 const App: React.SFC = function App() {
   return (
     <div>
-      Oi, put stuff here
+      {/* Once we add material-ui, the nav bar will probably replace this link */}
+      <Link to="/">Home</Link>
+      <Router>
+        {/* One component for each page/route goes in here */}
+      </Router>
     </div>
   );
 };
