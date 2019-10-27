43b89e0088413d8598e2f65f1c7aeb866d533e4d
Brandon Gathright <47648794+btgathright@users.noreply.github.com>
2019-10-27 14:32:36 -0500

    Add Department model

--- autoscheduler/scraper/models/department.py
+++ autoscheduler/scraper/models/department.py
@@ -1,4 +1,9 @@
 from django.db import models
 
 class Department(models.Model):
-    """ Describe model here """
+    """ Data structure that stores the department tag and description """
+    code = models.CharField(max_length=4, primary_key=True) # Ex: CSCE
+    description = models.TextField(max_length=100) # Ex: CSCE - Computer Sci & Engr
+
+    class Meta:
+        db_table = "departments"
