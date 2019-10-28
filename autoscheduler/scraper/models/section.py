c796e98c582644a171266f461fa9e4639cfb53a6
Ryan Conn <rconn478@gmail.com>
2019-10-28 18:01:41 +0100

    Changed IDs to bitint, improved formatting

--- autoscheduler/scraper/models/section.py
+++ autoscheduler/scraper/models/section.py
@@ -7,15 +7,14 @@ from django.db import models
 from scraper.models import Instructor
 
 class Section(models.Model):
-    """ Section contains data for a group of meetings.
-    """
-    id = models.IntegerField(primary_key=True) # id is primary key in scraped data
+    """ Section contains data for a group of meetings. """
+    id = models.BigIntegerField(primary_key=True) # id is primary key in scraped data
     subject = models.CharField(max_length=4, db_index=True)
     course_num = models.IntegerField(db_index=True)
     section_num = models.IntegerField(db_index=True)
     term_code = models.IntegerField(db_index=True)
     min_credits = models.IntegerField()
-    max_credits = models.IntegerField(null=True) #min_credits is never null, but max_credits can be
+    max_credits = models.IntegerField(null=True) # min_credits is never null, but max_credits can be
     max_enrollment = models.IntegerField()
     current_enrollment = models.IntegerField()
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
@@ -27,16 +26,13 @@ class Meeting(models.Model):
     """Describes a particular meeting time of a section.
     Each section has one or more meetings.
     """
-    id = models.IntegerField(primary_key=True) # id is primary key in scraped data
+    id = models.BigIntegerField(primary_key=True) # id is primary key in scraped data
     crn = models.IntegerField(db_index=True) # We'll probably going to be searching for CRNs a lot
     building = models.CharField(max_length=4, null=True) # Online/research meetings have no building
-    meeting_days = ArrayField(
-        models.BooleanField(),
-        size=7
-    )
+    meeting_days = ArrayField(models.BooleanField(), size=7)
     start_time = models.TimeField(null=True)
     end_time = models.TimeField(null=True)
-    meeting_type = models.CharField(max_length=3) #Meeting types: LEC, LAB, RES, INS
+    meeting_type = models.CharField(max_length=3) # Meeting types: LEC, LAB, RES, INS
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
 
     class Meta:
