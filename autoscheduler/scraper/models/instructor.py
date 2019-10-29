5643000117a9d4eb19452955b80b4189f71e9b55
Adel Hassan <sean.dashals@gmail.com>
2019-10-28 23:20:35 -0500

    Added instructor model

--- autoscheduler/scraper/models/instructor.py
+++ autoscheduler/scraper/models/instructor.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class Instructor(models.Model):
-    """ Describe model here """
+    """ Model that represents a professor, who may teach many courses """
+    id = models.IntegerField(primary_key=True)
+    email_address = models.CharField(max_length=320, null=True)
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        db_name = "instructors"
