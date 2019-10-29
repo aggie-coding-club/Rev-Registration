fc624a79a0c588815665bed4bcb070ed35f7572c
Adel Hassan <sean.dashals@gmail.com>
2019-10-29 00:00:52 -0500

    Created tests folder and added first test

---
+++ autoscheduler/frontend/src/tests/App.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react';
+
+import App from '../App';
+
+test('renders without errors', () => {
+    const { container } = render(<App />);
+    expect(container).toBeTruthy();
+});
