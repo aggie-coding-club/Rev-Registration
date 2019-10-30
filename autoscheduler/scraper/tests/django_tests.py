c9904c09d22fff7070be568ba2489bdd08397b58
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-10-30 02:02:57 -0500

    Added addopts=-ra to pytest.ini for test summaries

--- autoscheduler/scraper/tests/django_tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class DjangoTestCase(TestCase):
-    def test_thing(self):
-        self.assertEqual(1, 1)
--- autoscheduler/scraper/tests/unit_tests.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase
-
-class UnitTestCase(TestCase):
-    def test_unit(self):
-        self.assertEqual(1, 2)
