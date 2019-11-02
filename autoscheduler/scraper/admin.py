6a2081db05ad0964c674ebf37bc5d62a3d19dbbb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-11-02 17:12:09 -0500

    Added models to admin.py
    This allows them to be viewed in localhost:8000/admin, in case they ever
    want to

--- autoscheduler/scraper/admin.py
+++ autoscheduler/scraper/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register the models here, so we can see them in /admin
+from scraper.models import Section, Course, Meeting, Instructor, Department
+
+admin.site.register(Section)
+admin.site.register(Course)
+admin.site.register(Meeting)
+admin.site.register(Instructor)
+admin.site.register(Department)
