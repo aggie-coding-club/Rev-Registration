fd32db8d08d78f1c6a97157829ee6ad2eb4807eb
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-11-02 17:20:24 -0500

    Changed department id to be code + term composite
    Added term attribute
    Also added function to generate the id

--- autoscheduler/scraper/models/department.py
+++ autoscheduler/scraper/models/department.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+def generate_department_id(code: str, term: str):
+    """ Generates the department id as {code}-{term} """
+
+    return "".join((code, '-', term))
+
 class Department(models.Model):
     """ Data structure that stores the department tag and description """
-    code = models.CharField(max_length=4, primary_key=True) # Ex: CSCE
+
+    id = models.CharField(max_length=10, primary_key=True) # code + term
+    code = models.CharField(max_length=4) # Ex: CSCE
     description = models.TextField(max_length=100) # Ex: CSCE - Computer Sci & Engr
+    term = models.CharField(max_length=6, db_index=True)
 
     class Meta:
         db_table = "departments"
