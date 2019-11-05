032c64caff4c605b27b8d97509e0331ce8f549bc
Adel Hassan <sean.dashals@gmail.com>
2019-11-04 18:47:48 -0600

    Added 2 frontend types

--- autoscheduler/frontend/src/types.ts
+++ autoscheduler/frontend/src/types.ts
@@ -1,3 +1,32 @@
+/* eslint-disable import/prefer-default-export */
+
 /**
  * This file is where we will declare the types of each data-holding class we will be using.
  */
+export enum MeetingType {
+  LEC, LAB, RES, INS
+}
+
+export interface Meeting {
+  id: number;
+  crn: number;
+  building: string | null;
+  meetingDays: boolean[];
+  startTimeHours: number;
+  startTimeMinutes: number;
+  endTimeHours: number;
+  endTimeMinutes: number;
+  meetingType: MeetingType;
+  section: Section;
+}
+
+export interface Section {
+  id: number;
+  subject: string;
+  courseNum: number;
+  sectionNum: number;
+  minCredits: number;
+  maxCredits: number | null;
+  currentEnrollment: number;
+  instructor: number;
+}
