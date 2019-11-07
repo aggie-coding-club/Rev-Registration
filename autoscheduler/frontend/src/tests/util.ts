e8468c23e057991638d7df12fff1441b7a164d75
Adel Hassan <sean.dashals@gmail.com>
2019-11-06 20:58:12 -0600

    Added remaining tests for types

---
+++ autoscheduler/frontend/src/tests/util.ts
@@ -0,0 +1,17 @@
+import Instructor from '../types/Instructor';
+import Section from '../types/Section';
+
+/* eslint-disable import/prefer-default-export */
+
+/**
+ * Allows us to set arbitrary arguments of arguments for easier
+ * test-writing
+ */
+export interface Indexable {
+  [key: string]: number | string | boolean[] | Instructor | Section;
+}
+
+/**
+ * Mocks the uncertain types returned by HttpResponse.json()
+ */
+export const fetchMock = jest.fn((obj) => JSON.parse(JSON.stringify(obj)));
