a9c0885f91098340396ed61f9232e97a1cf16c25
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-11-12 00:38:31 -0600

    Added material-ui theme

--- autoscheduler/frontend/src/App.tsx
+++ autoscheduler/frontend/src/App.tsx
@@ -2,25 +2,29 @@ import * as React from 'react';
 import { Router, Link } from '@reach/router';
 import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
+import { ThemeProvider } from '@material-ui/styles';
 
+import theme from './theme';
 import Empty from './components/Empty';
 
 const App: React.SFC = function App() {
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit">
-            <Link to="/">
-              <HomeIcon />
-            </Link>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        {/* One component for each page/route goes in here */}
-        <Empty path="/" />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit">
+              <Link to="/">
+                <HomeIcon />
+              </Link>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Router>
+          {/* One component for each page/route goes in here */}
+          <Empty path="/" />
+        </Router>
+      </ThemeProvider>
     </div>
   );
 };
