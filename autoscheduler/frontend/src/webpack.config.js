3f8cd2bc2d0673ddffcb3428e2a8ed1ee7267f2f
Adel Hassan <sean.dashals@gmail.com>
2019-11-13 11:11:24 -0600

    Went back to LiveReload

--- autoscheduler/frontend/src/webpack.config.js
+++ autoscheduler/frontend/src/webpack.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const webpack = require('webpack');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   mode: 'production', // Should we change this to development?
@@ -31,7 +31,7 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new LiveReloadPlugin({ ignore: /tests/ }),
   ],
 
   // When importing a module whose path matches one of the following, just assume a corresponding
