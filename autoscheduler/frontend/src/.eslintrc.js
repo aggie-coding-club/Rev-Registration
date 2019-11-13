80fa9220caeedc4d365de170346bcfce1e434fca
Adel Hassan <sean.dashals@gmail.com>
2019-11-13 08:01:12 -0600

    Added Live Reload
    * Live reload for npm run dev
    
    * Switched to HotModuleReplacement
    
    * Fix linting
    
    * Went back to LiveReload
    
    * Modified package lock

--- autoscheduler/frontend/src/.eslintrc.js
+++ autoscheduler/frontend/src/.eslintrc.js
@@ -45,6 +45,5 @@ module.exports = {
   rules: {
     "react/jsx-filename-extension": [1, {"extensions": [".jsx", ".tsx"]}],
     "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.ts", "**/*.test.tsx", "**/*.config.js"]}],
-    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
   }
 };
