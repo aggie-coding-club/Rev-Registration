50083f6d19d3089d0ef2afe649c8c4872e6cbc32
christopher <christopherjmacgregor@gmail.com>
2019-11-15 17:41:19 -0600

    welcomepage components and creation foundation

--- autoscheduler/frontend/src/App.tsx
+++ autoscheduler/frontend/src/App.tsx
@@ -1,25 +1,22 @@
 import * as React from 'react';
 import { Router, Link } from '@reach/router';
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Button } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './theme';
 import Empty from './components/Empty';
+import NavBar from './Components/NavBar';
+import HelpText from './Components/HelpText';
+import SelectTerm from './Components/SelectTerm';
 
 const App: React.SFC = function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start">
-              <Link to="/">
-                <HomeIcon color="secondary" />
-              </Link>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+        <NavBar/>
+        <HelpText/>
+        <SelectTerm/>
         <Router>
           {/* One component for each page/route goes in here */}
           <Empty path="/" />
--- autoscheduler/frontend/src/index.tsx
+++ autoscheduler/frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
+// import App from './Components/App';
 import App from './App';
 
 ReactDom.render(
