c47b438ddfd1e9e2948413d52476bae6ec0c0ff0
Adel Hassan <sean.dashals@gmail.com>
2019-11-15 11:11:28 -0600

    Implemented suggestions for MeetingCard tests

--- autoscheduler/frontend/src/tests/MeetingCard.test.tsx
+++ autoscheduler/frontend/src/tests/MeetingCard.test.tsx
@@ -3,7 +3,8 @@ import '@testing-library/jest-dom/extend-expect';
 import * as React from 'react';
 
 import MeetingCard from '../components/MeetingCard';
-import { Meeting, MeetingType, Section } from '../types';
+import Meeting, { MeetingType } from '../types/Meeting';
+import Section from '../types/Section';
 
 const testSection: Section = {
   id: 123456,
@@ -29,8 +30,13 @@ const testMeeting: Meeting = {
   section: testSection,
 };
 
-test('accepts meeting as props', () => {
+test('accepts meeting as prop', () => {
   const { container } = render(<MeetingCard meeting={testMeeting} />);
+  expect(container).toBeTruthy();
+});
+
+test('accepts color as prop', () => {
+  const { container } = render(<MeetingCard meeting={testMeeting} color="#500000" />);
   expect(container).toBeTruthy();
 });
 
@@ -40,25 +46,7 @@ test('displays subject and course number', () => {
   expect(getByText(/121/)).toBeTruthy();
 });
 
-test('subject and course number can adapt to random values', () => {
-  // arrange
-  const randSubject = new Array(5).reduce((acc) => acc + String.fromCharCode(
-    Math.floor(65 + Math.random() * 26),
-  ));
-  const randCourseNum = Math.floor(100 + Math.random() * 400);
-  const randMeeting: Meeting = {
-    ...testMeeting,
-    section: {
-      ...testMeeting.section,
-      subject: randSubject,
-      courseNum: randCourseNum,
-    },
-  };
-
-  // act
-  const { getByText } = render(<MeetingCard meeting={randMeeting} />);
-
-  // assert
-  expect(getByText(randSubject)).toBeTruthy();
-  expect(getByText(randCourseNum.toString())).toBeTruthy();
+test('displays meeting type', () => {
+  const { getByText } = render(<MeetingCard meeting={testMeeting} />);
+  expect(getByText(/LEC/i)).toBeTruthy();
 });
