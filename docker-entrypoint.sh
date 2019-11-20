ff055e10af74d94f6d5907066f1d952dd605a989
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-11-20 12:34:28 -0600

    Updated pytest.init and made docker use pytest
    * Added postgres-info creation to docker entrypoint
    
    This is required to actually run the backend tests,
    since config.py will call sys.exit() if it's empty

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Note: This file MUST have LF line endings
 
+# While Docker doesn't actually use the postgres-info.json file, it must be filled out
+# otherwise, autoscheduler/config/config.py will call sys.exit() and end the tests before they can run
+echo "Creating empty postgres-info.json file"
+echo "{ \"user\": \"a\", \"password\": \"a\" }" > /app/autoscheduler/config/postgres-info.json
+
 echo "Creating migrations"
 python3 manage.py makemigrations --settings=autoscheduler.settings.docker scraper
 
@@ -8,4 +13,5 @@ echo "Migrating"
 python3 manage.py migrate --settings=autoscheduler.settings.docker scraper
 
 echo "Starting tests"
-python3 manage.py test --settings=autoscheduler.settings.docker
+export DJANGO_SETTINGS_MODULE=autoscheduler.settings.docker
+pytest
