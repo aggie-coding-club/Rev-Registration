6fa2c4c88d6e93530b7ebdbf60b69411cdd38ae9
Adel Hassan <sean.dashals@gmail.com>
2019-11-20 21:34:39 -0600

    Simpler regex

--- autoscheduler/frontend/urls.py
+++ autoscheduler/frontend/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, re_path
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    # match the root
-    path('', views.index),
-    # match all other pages
-    re_path(r'^(?:.*)/?$', views.index),
+    # match literally everything
+    re_path(r'.*', views.index),
 ]
