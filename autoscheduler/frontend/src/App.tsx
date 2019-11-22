23c3621d67e60f48b8d1899e88b58d4e372ad059
christopher <christopherjmacgregor@gmail.com>
2019-11-22 11:21:45 -0600

    landing page components have dedicated folder.

--- autoscheduler/frontend/src/App.tsx
+++ autoscheduler/frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Router } from '@reach/router';
 import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './theme';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/LandingPage/LandingPage';
+import NavBar from './components/NavBar';
 
 const App: React.SFC = function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
+        <NavBar />
         <Router>
           {/* One component for each page/route goes in here */}
           <LandingPage path="/" />
