097cc931a96a1faa2de2de0fec2afecbe8ed01ff
Adel Hassan <sean.dashals@gmail.com>
2019-11-23 21:50:43 -0600

    Added Redux meetings and tests

--- autoscheduler/frontend/src/redux/actions.ts
+++ autoscheduler/frontend/src/redux/actions.ts
@@ -3,3 +3,28 @@
  * return the action in the form that the Redux reducers need, with a string
  * type.
  */
+import Meeting from '../types/Meeting';
+import {
+  ADD_MEETING, REMOVE_MEETING, REPLACE_MEETINGS, SingleMeetingAction, MultiMeetingAction,
+} from './actionTypes';
+
+export function addMeeting(meeting: Meeting): SingleMeetingAction {
+  return {
+    type: ADD_MEETING,
+    meeting,
+  };
+}
+
+export function removeMeeting(meeting: Meeting): SingleMeetingAction {
+  return {
+    type: REMOVE_MEETING,
+    meeting,
+  };
+}
+
+export function replaceMeetings(meetings: Meeting[]): MultiMeetingAction {
+  return {
+    type: REPLACE_MEETINGS,
+    meetings,
+  };
+}
