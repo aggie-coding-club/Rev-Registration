b5bd420b4232a2865b5c454f96fd6656d0fd0da3
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2019-11-24 20:39:10 -0600

    Fixed some errors with previous code
    Corrected spelling of prerequisites
    Changed maxlength of id in course model so that the primary key can be stored
    Generate course id import now uses abosolute path
    Parse section now takes instructor as parameter
    Still have to give parse meeting a section parameter.

--- autoscheduler/scraper/models/course.py
+++ autoscheduler/scraper/models/course.py
@@ -8,20 +8,18 @@ def generate_course_id(dept: str, course_num: str, term: str):
 class Course(models.Model):
     """ Basic course model """
 
-    id = models.CharField(max_length=10, primary_key=True) # Could be dept+course_num
+    id = models.CharField(max_length=14, primary_key=True) # Could be dept+course_num
     dept = models.CharField(max_length=4, db_index=True) # CSCE
     course_num = models.CharField(max_length=5, db_index=True) # i.e. 314
     title = models.CharField(max_length=100) # Course title, i.e. "Programming Languages"
     description = models.TextField(blank=True)
-
     prerequisites = models.TextField(blank=True)
     corequisites = models.TextField(blank=True)
-
     cd = models.BooleanField() # states whether course is a cd credit
     icd = models.BooleanField() # states whether course is an icd credit
     core_curriculum = models.CharField(max_length=32, blank=True) # ex 'Creative Arts'
-
     credit_hours = models.FloatField(null=True) # Number of credit hours the course
+    term = models.CharField(max_length=6,blank=True)#term code
 
     class Meta:
         db_table = "courses"
