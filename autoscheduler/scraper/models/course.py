f6788c432fe432001520fb6eb2220ad88879c60e
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2019-11-26 11:50:53 -0600

    scrape_courses impovements
    -scrape_courses errors were fixed and it now runs succesfully from what I can tell at the moment.
    -created generate_meeting_id and generate_meeting_time functions in section.py
    -removed prereqs,coreqs,cd,and icd from course.py
    -instructor primary key is now their name
    -linting errors remain

--- autoscheduler/scraper/models/course.py
+++ autoscheduler/scraper/models/course.py
@@ -13,10 +13,6 @@ class Course(models.Model):
     course_num = models.CharField(max_length=5, db_index=True) # i.e. 314
     title = models.CharField(max_length=100) # Course title, i.e. "Programming Languages"
     description = models.TextField(blank=True)
-    prerequisites = models.TextField(blank=True)
-    corequisites = models.TextField(blank=True)
-    cd = models.BooleanField() # states whether course is a cd credit
-    icd = models.BooleanField() # states whether course is an icd credit
     core_curriculum = models.CharField(max_length=32, blank=True) # ex 'Creative Arts'
     credit_hours = models.FloatField(null=True) # Number of credit hours the course
     term = models.CharField(max_length=6,blank=True)#term code
