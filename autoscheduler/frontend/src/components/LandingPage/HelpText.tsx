c4b01a608b410b6508ea59532d929c5bf882c845
Adel Hassan <sean.dashals@gmail.com>
2019-11-29 15:51:56 -0600

    Resolved errors, highlight selected menu item

--- autoscheduler/frontend/src/components/LandingPage/HelpText.tsx
+++ autoscheduler/frontend/src/components/LandingPage/HelpText.tsx
@@ -21,25 +21,25 @@ const HelpText: React.SFC = function App() {
             Guidelines
           </Box>
         </Typography>
-        <Typography
-          variant="body1"
-          align="center"
-        >
-          <Box padding={2}>
-          Somebody once told me the world is gonna roll me
-          I ain&apos;t the sharpest tool in the shed
-          She was looking kind of dumb with her finger and her thumb
-          In the shape of an &quot;L&quot; on her forehead
-          Well the years start coming and they don&apos;t stop coming
-          Fed to the rules and I hit the ground running
-          Somebody once told me the world is gonna roll me
-          I ain&apos;t the sharpest tool in the shed
-          She was looking kind of dumb with her finger and her thumb
-          In the shape of an &quot;L&quot; on her forehead
-          Well the years start coming and they don&apos;t stop coming
-          Fed to the rules and I hit the ground running
-          </Box>
-        </Typography>
+        <Box padding={2}>
+          <Typography
+            variant="body1"
+            align="center"
+          >
+            Somebody once told me the world is gonna roll me
+            I ain&apos;t the sharpest tool in the shed
+            She was looking kind of dumb with her finger and her thumb
+            In the shape of an &quot;L&quot; on her forehead
+            Well the years start coming and they don&apos;t stop coming
+            Fed to the rules and I hit the ground running
+            Somebody once told me the world is gonna roll me
+            I ain&apos;t the sharpest tool in the shed
+            She was looking kind of dumb with her finger and her thumb
+            In the shape of an &quot;L&quot; on her forehead
+            Well the years start coming and they don&apos;t stop coming
+            Fed to the rules and I hit the ground running
+          </Typography>
+        </Box>
       </Paper>
     </Grid>
   );
