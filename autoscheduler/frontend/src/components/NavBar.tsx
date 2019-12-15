d87107002aaf727eae08394e7914b073b699356b
Adel Hassan <sean.dashals@gmail.com>
2019-12-15 12:21:19 -0600

    Removed menu button

--- autoscheduler/frontend/src/components/NavBar.tsx
+++ autoscheduler/frontend/src/components/NavBar.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import {
-  AppBar, Toolbar, Typography, Button, IconButton, makeStyles,
+  AppBar, Toolbar, Typography, Button, makeStyles,
 } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
 import appTheme from '../theme';
 
 const useStyles = makeStyles((theme) => ({
@@ -27,14 +26,6 @@ const NavBar: React.SFC = function App() {
         color="primary"
       >
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             className={classes.title}
