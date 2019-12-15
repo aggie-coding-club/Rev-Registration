078bac8ea56007968d0a1dab6bd51c122a10f66b
Adel Hassan <sean.dashals@gmail.com>
2019-12-15 12:45:06 -0600

    Fixed bug where menu wouldn't close

--- autoscheduler/frontend/src/components/LandingPage/HelpText.tsx
+++ autoscheduler/frontend/src/components/LandingPage/HelpText.tsx
@@ -3,46 +3,44 @@ import {
   Paper, Typography, Grid, Box,
 } from '@material-ui/core';
 
-const HelpText: React.SFC = function App() {
-  return (
-    <Grid
-      container
-      direction="row"
-      justify="space-around"
-      alignItems="center"
-      style={{ marginTop: '5%', marginBottom: '2%' }}
-    >
-      <Paper style={{ width: '55%', maxWidth: '800px' }}>
+const HelpText: React.SFC = () => (
+  <Grid
+    container
+    direction="row"
+    justify="space-around"
+    alignItems="center"
+    style={{ marginTop: '5%', marginBottom: '2%' }}
+  >
+    <Paper style={{ width: '55%', maxWidth: '800px' }}>
+      <Typography
+        variant="h3"
+        align="center"
+      >
+        <Box padding={2} paddingTop={4} paddingBottom={0}>
+            Guidelines
+        </Box>
+      </Typography>
+      <Box padding={2}>
         <Typography
-          variant="h3"
+          variant="body1"
           align="center"
         >
-          <Box padding={2}>
-            Guidelines
-          </Box>
+            Somebody once told me the world is gonna roll me
+            I ain&apos;t the sharpest tool in the shed
+            She was looking kind of dumb with her finger and her thumb
+            In the shape of an &quot;L&quot; on her forehead
+            Well the years start coming and they don&apos;t stop coming
+            Fed to the rules and I hit the ground running
+            Somebody once told me the world is gonna roll me
+            I ain&apos;t the sharpest tool in the shed
+            She was looking kind of dumb with her finger and her thumb
+            In the shape of an &quot;L&quot; on her forehead
+            Well the years start coming and they don&apos;t stop coming
+            Fed to the rules and I hit the ground running
         </Typography>
-        <Box padding={2}>
-          <Typography
-            variant="body1"
-            align="center"
-          >
-            Somebody once told me the world is gonna roll me
-            I ain&apos;t the sharpest tool in the shed
-            She was looking kind of dumb with her finger and her thumb
-            In the shape of an &quot;L&quot; on her forehead
-            Well the years start coming and they don&apos;t stop coming
-            Fed to the rules and I hit the ground running
-            Somebody once told me the world is gonna roll me
-            I ain&apos;t the sharpest tool in the shed
-            She was looking kind of dumb with her finger and her thumb
-            In the shape of an &quot;L&quot; on her forehead
-            Well the years start coming and they don&apos;t stop coming
-            Fed to the rules and I hit the ground running
-          </Typography>
-        </Box>
-      </Paper>
-    </Grid>
-  );
-};
+      </Box>
+    </Paper>
+  </Grid>
+);
 
 export default HelpText;
