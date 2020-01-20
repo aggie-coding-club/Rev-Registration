11410ea812b3e427bc898ff572b0d9f22be9337d
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-01-20 14:36:51 -0600

    Updated load_json with load_pdf function
    Also added a _generate_path function for use in it + load_json_file

--- autoscheduler/scraper/tests/utils/load_json.py
+++ autoscheduler/scraper/tests/utils/load_json.py
@@ -2,10 +2,18 @@ import json
 
 from pathlib import Path
 
-def load_json_file(path: str):
-    """ Loads a json file given a path """
+def _generate_path(path: str) -> Path:
+    """ Generates a Path object from a relative path """
+
     base_path = Path(__file__).parent
     file_path = (base_path / path).resolve()
+
+    return file_path
+
+def load_json_file(path: str):
+    """ Loads a json file given a path """
+
+    file_path = _generate_path(path)
 
     data = None
     with open(file_path) as json_file:
@@ -14,3 +22,10 @@ def load_json_file(path: str):
         json_file.close()
 
     return data
+
+def load_pdf(path: str):
+    """ Loads a pdf file given a path """
+
+    file_path = _generate_path(path)
+
+    return open(file_path, "rb")
