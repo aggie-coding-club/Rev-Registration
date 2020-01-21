ce7a22de61e1dc711eeb44d2c31dec17e0b485cf
Ryan Conn <rconn478@gmail.com>
2020-01-21 10:09:49 -0600

    Update models

--- autoscheduler/scraper/models/course.py
+++ autoscheduler/scraper/models/course.py
@@ -12,14 +12,7 @@ class Course(models.Model):
     dept = models.CharField(max_length=4, db_index=True) # CSCE
     course_num = models.CharField(max_length=5, db_index=True) # i.e. 314
     title = models.CharField(max_length=100) # Course title, i.e. "Programming Languages"
-    description = models.TextField(blank=True)
-
-    prerequisites = models.TextField(blank=True)
-    corequisites = models.TextField(blank=True)
-
-    cd = models.BooleanField() # states whether course is a cd credit
-    icd = models.BooleanField() # states whether course is an icd credit
-    core_curriculum = models.CharField(max_length=32, blank=True) # ex 'Creative Arts'
+    term = models.CharField(max_length=6)
 
     credit_hours = models.FloatField(null=True) # Number of credit hours the course
 
--- autoscheduler/scraper/models/instructor.py
+++ autoscheduler/scraper/models/instructor.py
@@ -2,9 +2,8 @@ from django.db import models
 
 class Instructor(models.Model):
     """ Model that represents a professor, who may teach many courses """
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(max_length=100, primary_key=True)
     email_address = models.CharField(max_length=48, null=True)
-    name = models.CharField(max_length=64)
 
     class Meta:
         db_table = "instructors"
