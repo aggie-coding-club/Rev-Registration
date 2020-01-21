be4970a6de3a1a20512f43b087555913b7d7f704
Ryan Conn <rconn478@gmail.com>
2020-01-21 16:45:19 -0600

    Updated course/section serializer and course view, fixed some test cases

--- autoscheduler/scraper/views.py
+++ autoscheduler/scraper/views.py
@@ -1,14 +1,14 @@
 from rest_framework import generics
-
 from .serializers import CourseSerializer
 from .models.course import Course
 
-class RetrieveCourseView(generics.ListAPIView):
+class RetrieveCourseView(generics.RetrieveAPIView):
     """ API endpoint for viewing course information """
     serializer_class = CourseSerializer
 
-    def get_queryset(self):
+    def get_object(self):
+        """ Overrides default behavior of get_object() to work without a primary key """
         dept = self.request.query_params.get('dept')
         course_num = self.request.query_params.get('course_num')
         term = self.request.query_params.get('term')
-        return Course.objects.filter(dept=dept, course_num=course_num, term=term)
+        return Course.objects.get(dept=dept, course_num=course_num, term=term)
