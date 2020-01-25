f9dd88ed83e4cb4db9bcbee360663904fb3683cc
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-01-24 19:39:24 -0600

    Refactored scrape_courses, mainly variable names
    Shortened variables like "section_subject" => "subject"
    
    Also changed / elaborated on some comments
    
    Also added return / parameter types for model arguments (i.e. Instructor, Section)

--- autoscheduler/scraper/management/commands/scrape_courses.py
+++ autoscheduler/scraper/management/commands/scrape_courses.py
@@ -8,8 +8,8 @@ from scraper.models import Course, Instructor, Section, Meeting, Department
 from scraper.models.course import generate_course_id
 from scraper.models.section import generate_meeting_id
 
-def convert_meeting_time(string_time: str):
-    """ Converts a meeting time from a string in format hhmm to datetime.
+def convert_meeting_time(string_time: str) -> datetime.time:
+    """ Converts a meeting time from a string in format hhmm to datetime.time object.
         ex) 1245 = 12:45am. 1830 = 6:30 pm.
     """
 
@@ -22,7 +22,7 @@ def convert_meeting_time(string_time: str):
 
     return meeting_time
 
-def parse_meeting_days(meetings_data):
+def parse_meeting_days(meetings_data) -> List[bool]:
     """ Generates a list of seven booleans where each corresponds to a day in the week.
         The first is Monday and the last is Sunday. If true, there is class that day
     """
@@ -32,61 +32,58 @@ def parse_meeting_days(meetings_data):
 
     return attend_days
 
-def parse_section(course_data, instructor_object):
+def parse_section(course_data, instructor: Instructor):
     """ Puts section data in database & calls parse_meeting.
         Called from parse_course.
     """
 
-    #gets values to create object
     section_id = int(course_data['id'])
-    section_subject = course_data['subject']
+    subject = course_data['subject']
     course_number = course_data['courseNumber']
     section_number = course_data['sequenceNumber']
-    section_term_code = course_data['term']
+    term_code = course_data['term']
 
+    crn = 0
     if len(course_data['meetingsFaculty']) > 0:
-        section_crn = course_data['meetingsFaculty'][0]['courseReferenceNumber']
-    else:
-        section_crn = 0
+        crn = course_data['meetingsFaculty'][0]['courseReferenceNumber']
 
-    section_min_credits = course_data['creditHourLow']
-    section_max_credits = course_data['creditHourHigh']
-    section_max_enrollment = course_data['maximumEnrollment']
-    section_current_enrollment = course_data['enrollment']
+    min_credits = course_data['creditHourLow']
+    max_credits = course_data['creditHourHigh']
+    max_enrollment = course_data['maximumEnrollment']
+    current_enrollment = course_data['enrollment']
 
     # Creates and saves section object
-    section_object = Section(
-        id=section_id, subject=section_subject, course_num=course_number,
-        section_num=section_number, term_code=section_term_code,
-        crn=section_crn, min_credits=section_min_credits,
-        max_credits=section_max_credits, max_enrollment=section_max_enrollment,
-        current_enrollment=section_current_enrollment, instructor=instructor_object)
-    section_object.save()
+    section_model = Section(
+        id=section_id, subject=subject, course_num=course_number,
+        section_num=section_number, term_code=term_code,
+        crn=crn, min_credits=min_credits,
+        max_credits=max_credits, max_enrollment=max_enrollment,
+        current_enrollment=current_enrollment, instructor=instructor)
+    section_model.save()
 
-    # Parse each meeting in this section
+    # Parse each meeting in this section. i is the counter used to identify each Meeting
     for i, meetings_data in enumerate(course_data['meetingsFaculty']):
-        parse_meeting(meetings_data, section_object, i)
+        parse_meeting(meetings_data, section_model, i)
 
-def parse_meeting(meetings_data, section_object, meeting_count):
+def parse_meeting(meetings_data, section: Section, meeting_count: int):
     """ Parses the meeting data and saves it as a Meeting model.
         Called by parse_section on each of the section's meeting times.
     """
 
-    meeting_id = generate_meeting_id(str(section_object.id), str(meeting_count))
+    meeting_id = generate_meeting_id(str(section.id), str(meeting_count))
 
-    meeting_class_days = parse_meeting_days(meetings_data)
+    class_days = parse_meeting_days(meetings_data)
 
-    meeting_start_time = convert_meeting_time(meetings_data['meetingTime']['beginTime'])
-    meeting_end_time = convert_meeting_time(meetings_data['meetingTime']['endTime'])
+    start_time = convert_meeting_time(meetings_data['meetingTime']['beginTime'])
+    end_time = convert_meeting_time(meetings_data['meetingTime']['endTime'])
 
-    meeting_building = meetings_data['meetingTime']['building']
-    meeting_class_type = meetings_data['meetingTime']['meetingType']
+    building = meetings_data['meetingTime']['building']
+    class_type = meetings_data['meetingTime']['meetingType']
 
-    meeting_object = Meeting(id=meeting_id, building=meeting_building,
-                             meeting_days=meeting_class_days,
-                             start_time=meeting_start_time, end_time=meeting_end_time,
-                             meeting_type=meeting_class_type, section=section_object)
-    meeting_object.save()
+    meeting_model = Meeting(id=meeting_id, building=building, meeting_days=class_days,
+                            start_time=start_time, end_time=end_time,
+                            meeting_type=class_type, section=section)
+    meeting_model.save()
 
 def parse_instructor(course_data):
     """ Parses the instructor data and saves it as a Instructor model.
@@ -95,42 +92,43 @@ def parse_instructor(course_data):
 
     # Can have multiple instructor entries, although most will have 0-1
     for i in course_data['faculty']:
-        instructor_id = i['displayName'] if i['displayName'] else None
-        if instructor_id is None:
+        name = i['displayName'] if i['displayName'] else None
+
+        if name is None:
             return None
-        instructor_email = i['emailAddress']
 
-        instructor_object = Instructor(
-            id=instructor_id, email_address=instructor_email)
+        email = i['emailAddress']
 
-        instructor_object.save()
-        return instructor_object
+        instructor_model = Instructor(id=name, email_address=email)
+        instructor_model.save()
+
+        return instructor_model
 
 def parse_course(course_data):
     """ Creates Course model and saves it to the databsae.
         Calls parse_instructor and parse_section
     """
 
-    course_dept = course_data['subject']
+    dept = course_data['subject']
     course_number = course_data['courseNumber']
-    course_term_code = course_data['term']
-    # Generate the course's id using the above data
-    course_id = generate_course_id(course_dept, course_number, course_term_code)
+    term_code = course_data['term']
 
-    course_title = course_data['courseTitle']
-    course_credit_hours = course_data['creditHourLow']
+    # Generate the course's id using the above data
+    course_id = generate_course_id(dept, course_number, term_code)
+
+    title = course_data['courseTitle']
+    credit_hours = course_data['creditHourLow']
 
     #creates and saves course object
-    course_object = Course(
-        id=course_id, dept=course_dept, course_num=course_number, title=course_title,
-        credit_hours=course_credit_hours, term=course_term_code)
-    course_object.save()
+    course_model = Course(id=course_id, dept=dept, course_num=course_number,
+                          title=title, credit_hours=credit_hours, term=term_code)
+    course_model.save()
 
-    #calls other parse functions
-    instructor_object = parse_instructor(course_data)
-    parse_section(course_data, instructor_object)
+    # Parse the instructor, then send the returned Instructor model to parse_section
+    instructor_model = parse_instructor(course_data)
+    parse_section(course_data, instructor_model)
 
-def get_department_names(term_code) -> List[str]:
+def get_department_names(term_code: str) -> List[str]:
     """ Queries database for list of all departments """
     return [dept.code for dept in Department.objects.filter(term=term_code)]
 
