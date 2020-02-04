ad380e9e18394d673b17f494b058f7a7b055bf2f
Adel Hassan <sean.dashals@gmail.com>
2020-02-04 08:45:41 -0600

    Connected delete icon to Redux

--- autoscheduler/frontend/src/components/AvailabilityCard/AvailabilityCard.tsx
+++ autoscheduler/frontend/src/components/AvailabilityCard/AvailabilityCard.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Availability from '../../types/Availability';
 import ScheduleCard from '../ScheduleCard/ScheduleCard';
 import * as styles from './AvailabilityCard.css';
+import { deleteAvailability } from '../../redux/actions';
 
 interface AvailabilityCardProps {
     availability: Availability;
@@ -12,26 +14,42 @@ interface AvailabilityCardProps {
 
 const AvailabilityCard: React.FC<AvailabilityCardProps> = (
   { availability, firstHour, lastHour },
-) => (
-  <ScheduleCard
-    startTimeHours={availability.startTimeHours}
-    startTimeMinutes={availability.startTimeMinutes}
-    endTimeHours={availability.endTimeHours}
-    endTimeMinutes={availability.endTimeMinutes}
-    firstHour={firstHour}
-    lastHour={lastHour}
-    borderColor="red"
-    backgroundColor="#f4433680"
-  >
-    <div className={styles.container}>
-      <span style={{ color: 'black' }}>BUSY</span>
-      <DeleteIcon
-        htmlColor="rgba(0, 0, 0, 0.7)"
-        fontSize="small"
-        className={styles.icon}
-      />
-    </div>
-  </ScheduleCard>
-);
+) => {
+  const dispatch = useDispatch();
+  const {
+    available, dayOfWeek, startTimeHours, startTimeMinutes, endTimeHours, endTimeMinutes,
+  } = availability;
+
+  return (
+    <ScheduleCard
+      startTimeHours={startTimeHours}
+      startTimeMinutes={startTimeMinutes}
+      endTimeHours={endTimeHours}
+      endTimeMinutes={endTimeMinutes}
+      firstHour={firstHour}
+      lastHour={lastHour}
+      borderColor="red"
+      backgroundColor="#f4433680"
+    >
+      <div className={styles.container}>
+        <span style={{ color: 'black' }}>BUSY</span>
+        <DeleteIcon
+          htmlColor="rgba(0, 0, 0, 0.7)"
+          fontSize="small"
+          className={styles.icon}
+          onClick={(evt): void => {
+            dispatch(deleteAvailability({
+              available,
+              dayOfWeek,
+              time1: startTimeHours * 60 + startTimeMinutes,
+              time2: endTimeHours * 60 + endTimeMinutes,
+            }));
+            evt.preventDefault();
+          }}
+        />
+      </div>
+    </ScheduleCard>
+  );
+};
 
 export default AvailabilityCard;
