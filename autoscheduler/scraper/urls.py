13497c3ec7c82702e1fb4616ef1657549f67bfe6
Brandon Gathright <47648794+btgathright@users.noreply.github.com>
2020-02-09 13:52:47 -0600

    Implemented section api scraping

--- autoscheduler/scraper/urls.py
+++ autoscheduler/scraper/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from scraper.views import RetrieveCourseView
+from scraper.views import RetrieveSectionView
 
 urlpatterns = [
     path('course', RetrieveCourseView.as_view()),
+    path('sections', RetrieveSectionView.as_view())
 ]
--- autoscheduler/scraper/views.py
+++ autoscheduler/scraper/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .serializers import CourseSerializer
+from .serializers import SectionSerializer
 from .models.course import Course
+from .models.section import Section
 
 class RetrieveCourseView(generics.RetrieveAPIView):
     """ API endpoint for viewing course information, used by /api/course.
@@ -14,3 +16,16 @@ class RetrieveCourseView(generics.RetrieveAPIView):
         course_num = self.request.query_params.get('course_num')
         term = self.request.query_params.get('term')
         return Course.objects.get(dept=dept, course_num=course_num, term=term)
+
+class RetrieveSectionView(generics.RetrieveAPIView):
+    """ API endpoint for viewing course information, used by /api/sections.
+        This view returns a serialized section, should return .
+    """
+    serializer_class = SectionSerializer
+
+    def get_object(self):
+        """ Overrides default behavior of get_object() to work without a primary key """
+        dept = self.request.query_params.get('dept')
+        course_num = self.request.query_params.get('course_num')
+        term = self.request.query_params.get('term')
+        return Section.objects.get(dept=dept, course_num=course_num, term=term)
