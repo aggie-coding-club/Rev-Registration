dadf9321f4696974b2b435aa96a44066afde7b58
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-02-10 13:36:47 -0600

    Changed generate_path to be public function in load_json.py
    ALso removed redundant json.close() in load_json_file and instead returned it directly

--- autoscheduler/scraper/tests/utils/load_json.py
+++ autoscheduler/scraper/tests/utils/load_json.py
@@ -2,7 +2,7 @@ import json
 
 from pathlib import Path
 
-def _generate_path(path: str) -> Path:
+def generate_path(path: str) -> Path:
     """ Generates a Path object from a relative path """
 
     base_path = Path(__file__).parent
@@ -13,19 +13,14 @@ def _generate_path(path: str) -> Path:
 def load_json_file(path: str):
     """ Loads a json file given a path """
 
-    file_path = _generate_path(path)
+    file_path = generate_path(path)
 
-    data = None
     with open(file_path) as json_file:
-        data = json.load(json_file)
-
-        json_file.close()
-
-    return data
+        return json.load(json_file)
 
 def load_pdf(path: str):
     """ Loads a pdf file given a path """
 
-    file_path = _generate_path(path)
+    file_path = generate_path(path)
 
     return open(file_path, "rb")
