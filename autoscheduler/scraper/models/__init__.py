4d2c644af581df648a9fd30288fe335a00532303
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-02-11 13:40:58 -0600

    Added Grades model + GradeManager
    GradeManager is used for calculating an instructor's past grade distributions

--- autoscheduler/scraper/models/__init__.py
+++ autoscheduler/scraper/models/__init__.py
@@ -2,5 +2,6 @@ from .department import Department
 from .instructor import Instructor
 from .course import Course
 from .section import Section, Meeting
+from .grades import Grades
 
-__all__ = ["Department", "Instructor", "Course", "Section", "Meeting"]
+__all__ = ["Department", "Instructor", "Course", "Section", "Meeting", "Grades"]
