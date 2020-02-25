965349d84ec8b3b9d06f0e96754e8f50483ae001
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2020-02-25 16:49:20 -0600

    api/course/search bugs fixed
    -contains changed to startswith so that when 201 is in the 'search' parameter it doesn't return every course that takes in a 201X term instead of courses with the number 201. However, department must be inputted before the course number now.

--- autoscheduler/scraper/views.py
+++ autoscheduler/scraper/views.py
@@ -1,10 +1,40 @@
 from collections import OrderedDict
 from rest_framework import generics
 from rest_framework.response import Response
+from .serializers import CourseSerializer
+from .serializers import SectionSerializer
 from .serializers import TermSerializer
 from .serializers import CourseSearchSerializer
 from .models.course import Course
+from .models.section import Section
 from .models.department import Department
+
+class RetrieveCourseView(generics.RetrieveAPIView):
+    """ API endpoint for viewing course information, used by /api/course.
+        This view returns a serialized course, should return its title and credit hours.
+    """
+    serializer_class = CourseSerializer
+
+    def get_object(self):
+        """ Overrides default behavior of get_object() to work without a primary key """
+        dept = self.request.query_params.get('dept')
+        course_num = self.request.query_params.get('course_num')
+        term = self.request.query_params.get('term')
+        return Course.objects.get(dept=dept, course_num=course_num, term=term)
+
+class ListSectionView(generics.ListAPIView):
+    """ API endpoint for viewing course information, used by /api/sections.
+        This view returns a serialized section, should
+        return list of all sections for a given course.
+    """
+    serializer_class = SectionSerializer
+
+    def get_queryset(self):
+        """ Overrides default behavior of get_queryset() to work without a primary key """
+        dept = self.request.query_params.get('dept')
+        course_num = self.request.query_params.get('course_num')
+        term = self.request.query_params.get('term')
+        return Section.objects.filter(subject=dept, course_num=course_num, term_code=term)
 
 class RetrieveTermView(generics.ListAPIView):
     """ API endpoint for viewing terms, used by /api/term.
@@ -32,7 +62,7 @@ class RetrieveCourseSearchView(generics.ListAPIView):
             search and term parameter in the url"""
         search = self.request.query_params.get('search')
         term = self.request.query_params.get('term')
-        return Course.objects.filter(id__contains=search, term=term)
+        return Course.objects.filter(id__startswith=search.replace(" ", ""), term=term)
 
     def list(self, request):
         """Overrides default behavior of list method so terms are ouput in
