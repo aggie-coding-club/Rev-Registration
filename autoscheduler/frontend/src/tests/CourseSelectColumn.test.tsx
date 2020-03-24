cbce131449cd6ca37ae5e0a4d8948c7b0612ffa2
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-03-24 13:24:39 -0500

    Added CourseSelectColumn UI tests
    Made tests to use query/getAllByText (squashed)
    
    Inverted CourseSelectedColumn test+describes (squashed)

---
+++ autoscheduler/frontend/src/tests/CourseSelectColumn.test.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+
+import { render, fireEvent, act } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import autoSchedulerReducer from '../redux/reducers';
+import CourseSelectColumn from '../components/CourseSelectColumn/CourseSelectColumn';
+
+describe('CourseSelectColumn', () => {
+  describe('Adds a course card', () => {
+    test('wehn the Add Course button is clicked', () => {
+      // arrange
+      const store = createStore(autoSchedulerReducer, applyMiddleware(thunk));
+
+      const { getByText, getAllByText } = render(
+        <Provider store={store}>
+          <CourseSelectColumn />
+        </Provider>,
+      );
+
+      // act
+      // Press the button
+      act(() => { fireEvent.click(getByText('Add Course')); });
+
+      // Get the course cards
+      const cardsCount = getAllByText('Remove').length;
+
+      // assert
+      // There should be now be two since it defaults to one at the beginning
+      expect(cardsCount).toEqual(2);
+    });
+  });
+
+  describe('Removes a course card', () => {
+    test('when Remove is clicked on a course card', () => {
+      // arrange
+      const store = createStore(autoSchedulerReducer, applyMiddleware(thunk));
+
+      const { getByText, queryAllByText } = render(
+        <Provider store={store}>
+          <CourseSelectColumn />
+        </Provider>,
+      );
+
+      // act
+      // Press the button
+      act(() => { fireEvent.click(getByText('Remove')); });
+
+      const cardsCount = queryAllByText('Remove').length;
+
+      // assert
+      // Starts with 1 by default, so removing one should make it 0
+      expect(cardsCount).toEqual(0);
+    });
+  });
+});
