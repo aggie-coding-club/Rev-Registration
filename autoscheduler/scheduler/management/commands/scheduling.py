fbd3afc371b8103c62d954178d189727243c5628
Ryan Conn <rconn478@gmail.com>
2020-03-28 13:04:26 -0500

    Updated scheduling algorithm to work with unavailable times
    Performance improvements from changing meetings to tuple instead of dict
    
    Decreased query size by using only() to select needed columns

--- autoscheduler/scheduler/management/commands/scheduling.py
+++ autoscheduler/scheduler/management/commands/scheduling.py
@@ -1,19 +1,22 @@
 from itertools import islice, groupby
+from datetime import time
 from typing import Dict, Iterable, List, Tuple
 from django.core.management import base
 from scraper.models import Meeting, Section
-from scheduler.utils.random_product import random_product
+from scheduler.utils import random_product, UnavailableTime
 
-def _get_sections(course: Tuple[str, str], term: str) -> Dict[str, Tuple[Meeting]]:
-    """ Gets all sections for each course in courses, and organizes them by section_num
+def _get_sections(course: Tuple[str, str], term: str,
+                  unavailable_times: List[UnavailableTime]) -> Dict[str, Tuple[Meeting]]:
+    """ Gets all sections and meetings for each course in courses, and organizes them
+        by section_num
 
     Args:
         course: Tuple of (subject, course_num) to find sections for
         term: Term to find sections for
 
     Returns:
-        A dict of sections for the course with the meeting number as the key
-        and attributes for each meeting as the value
+        A dict of sections for the course with the section number as the key
+        and attributes for each meeting in the section as the value
     """
     subject, course_num = course
 
@@ -28,22 +31,33 @@ def _get_sections(course: Tuple[str, str], term: str) -> Dict[str, Tuple[Meeting
     # Get meetings based on sections of the course and order them by end time
     meetings = (Meeting.objects.filter(section_id__in=section_ids)
                 # Must be ordered by section id or groupby() doesn't work
-                .order_by('section_id'))
+                .order_by('section_id')
+                .only('start_time', 'end_time', 'section_id', 'meeting_days'))
+
     # Convert meeting_days into a set to check meeting days efficiently
     for meeting in meetings:
         meeting.meeting_days = set(i for i, day in enumerate(meeting.meeting_days) if day)
 
-    return {section_nums[section_num]: tuple(meetings)
-            for section_num, meetings in groupby(meetings, key=lambda m: m.section_id)}
+    # Organize meetings by section number
+    meetings = {section_nums[section_id]: tuple(meetings)
+                for section_id, meetings in groupby(meetings, key=lambda m: m.section_id)}
 
-def _partial_schedule_valid(courses: Iterable[Tuple[str, str]],
-                            meetings: Dict[str, Dict[str, Iterable[Meeting]]],
-                            schedule: Tuple[int], added_i: int) -> bool:
+    # Filter sections incompatible with unavailable_times by trying to create a "schedule"
+    # containing the section and unavailable times
+    compatibility = ({"unavailable": unavailable_times}, meetings)
+    to_delete = tuple(section for section in meetings
+                      if not _schedule_valid(compatibility, ("unavailable", section)))
+    for section in to_delete:
+        del meetings[section]
+
+    return meetings
+
+def _partial_schedule_valid(meetings: Tuple[Dict[str, Iterable[Meeting]]],
+                            schedule: Tuple[str], added_i: int) -> bool:
     """ Returns whether or not a schedule containing the chosen sections is valid
         after adding the course at index added_i
 
     Args:
-        courses: Iterable containing tuple of (subject, course number)
         meetings: Dict mapping courses to section numbers to meetings
         schedule: Sections to check
         addded_i: index of class added to schedule, used to avoid checking all meetings
@@ -52,9 +66,9 @@ def _partial_schedule_valid(courses: Iterable[Tuple[str, str]],
     Returns:
         Whether the schedule is valid up to index added_i
     """
-    added_section = meetings[courses[added_i]][schedule[added_i]]
+    added_section = meetings[added_i][schedule[added_i]]
     for i, section in islice(enumerate(schedule), None, added_i):
-        checking_section = meetings[courses[i]][section]
+        checking_section = meetings[i][section]
         for first_meeting in added_section:
             first_start = first_meeting.start_time
             first_end = first_meeting.end_time
@@ -73,26 +87,25 @@ def _partial_schedule_valid(courses: Iterable[Tuple[str, str]],
                     return False
     return True
 
-def _schedule_valid(courses: Iterable[Tuple[str, str]],
-                    meetings: Dict[str, Dict[str, Iterable[Meeting]]],
-                    schedule: List[str]) -> bool:
+def _schedule_valid(meetings: Tuple[Dict[str, Tuple[Meeting]]],
+                    schedule: Tuple[str]) -> bool:
     """ Returns whether or not a schedule containing the sections in schedule
         is valid. Sections should be in the same order as courses, and assumed valid
 
     Args:
-        courses: Iterable containing tuple of (subject, course number)
-        meetings: Dict mapping courses to section numbers to meetings
+        meetings: tuple of dicts mapping section numbers to meetings
         schedule: list of section numbers to check for compatibility
 
     Returns:
         Whether or not a schedule containing the given sections is valid
     """
     for i in range(1, len(schedule)):
-        if not _partial_schedule_valid(courses, meetings, schedule, i):
+        if not _partial_schedule_valid(meetings, schedule, i):
             return False
     return True
 
-def create_schedules(courses: List[Tuple[str, str]], term: str, num_schedules: int = 10):
+def create_schedules(courses: List[Tuple[str, str]], term: str,
+                     unavailable_times: List[UnavailableTime], num_schedules: int = 10):
     """ Generates and returns a schedule containing the courses provided as an argument.
 
     Args:
@@ -103,15 +116,15 @@ def create_schedules(courses: List[Tuple[str, str]], term: str, num_schedules: i
     Returns:
         Nothing, in the future this may return the section objects for generated schedules
     """
-    # Create dict mapping courses to sections to meetings
-    meetings = {course: _get_sections(course, term) for course in courses}
+    # meetings: Tuple mapping sections to meetings for each course
+    meetings = tuple(_get_sections(course, term, unavailable_times) for course in courses)
     # Get valid section numbers for each course
-    valid_choices = tuple(tuple(meetings[course]) for course in courses)
+    valid_choices = tuple(tuple(meetings[i]) for i in range(len(courses)))
 
     schedules = []
     # Generate random arrangements of sections and create schedules
     for schedule in random_product(*valid_choices):
-        if _schedule_valid(courses, meetings, schedule):
+        if _schedule_valid(meetings, schedule):
             schedules.append(schedule)
             if len(schedules) >= num_schedules:
                 break
@@ -127,4 +140,6 @@ class Command(base.BaseCommand):
         courses = [("COMM", "203"), ("CSCE", "121"), ("CSCE", "411"), ("MATH", "151"),
                    ("CSCE", "181")]
         term = "201911"
-        create_schedules(courses, term)
+        unavailable_times = [UnavailableTime(time(8), time(8, 50), 2),
+                             UnavailableTime(time(8), time(8, 50), 3)]
+        create_schedules(courses, term, unavailable_times)
