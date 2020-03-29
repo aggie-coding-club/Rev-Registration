4bf0a56e8293502d34103933aa559e7efb177aa8
Ryan Conn <rconn478@gmail.com>
2020-03-29 14:53:02 -0500

    Added documentation for what numbers in meeting_days correspond to
    Renamed _get_sections to _get_meetings to better represent what it does
    
    Simplified logic for comparing meeting days

--- autoscheduler/scraper/models/section.py
+++ autoscheduler/scraper/models/section.py
@@ -36,6 +36,7 @@ class Meeting(models.Model):
     """
     id = models.BigIntegerField(primary_key=True) # id is primary key in scraped data
     building = models.CharField(max_length=5, null=True)
+    # meeting_days[0] is Monday, meeting_days[6] is Sunday
     meeting_days = ArrayField(models.BooleanField(), size=7)
     start_time = models.TimeField(null=True)
     end_time = models.TimeField(null=True)
