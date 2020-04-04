c107346b5cdc4c3ce12c5f1d92f2011b02d615b1
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-04-04 16:59:43 -0500

    Fixed SelectTerm tests to use Redux store
    Also adds arrange/act/assert comments to match our standard testing setup

--- autoscheduler/frontend/src/tests/SelectTerm.test.tsx
+++ autoscheduler/frontend/src/tests/SelectTerm.test.tsx
@@ -1,27 +1,58 @@
 import { render, fireEvent } from '@testing-library/react';
 import * as React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import SelectTerm from '../components/LandingPage/SelectTerm/SelectTerm';
+import autoSchedulerReducer from '../redux/reducer';
 
 test('Menu is closed initially', () => {
-  render(<SelectTerm />);
+  // arrange/act
+  const store = createStore(autoSchedulerReducer);
 
+  render(
+    <Provider store={store}>
+      <SelectTerm />
+    </Provider>,
+  );
+
+  // assert
   expect(document.getElementsByClassName('MuiPopover-root')[0]).toHaveAttribute('aria-hidden');
 });
 
 test('Menu opens after button is clicked', () => {
-  const { getByText } = render(<SelectTerm />);
+  // arrange
+  const store = createStore(autoSchedulerReducer);
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <SelectTerm />
+    </Provider>,
+  );
+
+  // act
   const button = getByText('Select Term');
   fireEvent.click(button);
 
+  // assert
   expect(document.getElementsByClassName('MuiPopover-root')[0]).not.toHaveAttribute('aria-hidden');
 });
 
 test('Menu closes after item is selected', () => {
-  const { getByText } = render(<SelectTerm />);
+  // arrange
+  const store = createStore(autoSchedulerReducer);
+
+  const { getByText } = render(
+    <Provider store={store}>
+      <SelectTerm />
+    </Provider>,
+  );
+
+  // act
   const button = getByText('Select Term');
   fireEvent.click(button);
   const testSemester = getByText('Semester 1');
   fireEvent.click(testSemester);
 
+  // assert
   expect(document.getElementsByClassName('MuiPopover-root')[0]).toHaveAttribute('aria-hidden');
 });
