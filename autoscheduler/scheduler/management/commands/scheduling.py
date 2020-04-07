f41da87d57149378286104c18ed111bb1d4af2ff
Ryan Conn <rconn478@gmail.com>
2020-04-06 23:03:06 -0500

    Updated scheduling command to demonstrate how filters work
    Fixed tests
    
    Separated random_product tests into their own file
    
    Updated create_schedules test assertions to compare sets

--- autoscheduler/scheduler/management/commands/scheduling.py
+++ autoscheduler/scheduler/management/commands/scheduling.py
@@ -1,7 +1,7 @@
 import datetime
 from time import time
 from django.core.management import base
-from scheduler.utils import UnavailableTime
+from scheduler.utils import CourseFilter, UnavailableTime
 from scheduler.create_schedules import create_schedules
 
 class Command(base.BaseCommand):
@@ -9,13 +9,17 @@ class Command(base.BaseCommand):
 
     def handle(self, *args, **options):
         # Setup data to create schedule from
-        courses = [("COMM", "203"), ("CSCE", "121"), ("CSCE", "411"), ("MATH", "151"),
-                   ("CSCE", "181")]
+        course_names = [("COMM", "203"), ("CSCE", "121"), ("CSCE", "411"),
+                        ("MATH", "151"), ("CSCE", "181")]
+        courses = [CourseFilter(subject, course_num)
+                   for subject, course_num in course_names]
+        courses[3] = CourseFilter("ACCT", "229", honors=True)
         term = "201911"
         unavailable_times = [UnavailableTime(datetime.time(8), datetime.time(8, 50), 2),
                              UnavailableTime(datetime.time(8), datetime.time(8, 50), 3)]
+
         start = time()
         schedules = create_schedules(courses, term, unavailable_times)
         end = time()
-        print(f"Took {end - start} seconds to create schedules")
+        print(f"Took {end - start:.4f} seconds to create schedules")
         print(schedules)
