2fe7245a2cab1dde8dbcde3807dfe254cacc1832
Adel Hassan <sean.dashals@gmail.com>
2020-04-07 17:36:03 -0500

    Loads mocked schedule on button press

--- autoscheduler/frontend/src/components/App/App.tsx
+++ autoscheduler/frontend/src/components/App/App.tsx
@@ -5,28 +5,11 @@ import { useDispatch } from 'react-redux';
 
 import theme from '../../theme';
 import * as styles from './App.css';
-import fetchSavedSchedule from './testMeetings';
 import NavBar from '../NavBar';
 import LandingPage from '../LandingPage/LandingPage';
-import Test from './Test';
 import SchedulingPage from '../SchedulingPage/SchedulingPage';
-import { addSchedule } from '../../redux/actions/schedules';
-import selectSchedule from '../../redux/actions/selectedSchedule';
 
 const App: React.SFC = function App() {
-  // connect to Redux store
-  const dispatch = useDispatch();
-
-  // load initial schedule from server
-  React.useEffect(() => {
-    fetchSavedSchedule().then(
-      (meetings) => {
-        dispatch(addSchedule(meetings));
-        dispatch(selectSchedule(0));
-      },
-    );
-  }, []);
-
   return (
     <div className={styles.appContainer}>
       <ThemeProvider theme={theme}>
