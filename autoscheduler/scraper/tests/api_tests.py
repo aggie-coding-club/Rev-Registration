dd637fd7f8e9319b34639a9a8742ac5e5a64ce43
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-04-09 13:29:54 -0500

    Added enrollment sending from api/sections
    Also updates the tests to pass

--- autoscheduler/scraper/tests/api_tests.py
+++ autoscheduler/scraper/tests/api_tests.py
@@ -8,8 +8,6 @@ from scraper.serializers import (CourseSerializer, SectionSerializer, TermSerial
 
 class APITests(APITestCase): #pylint: disable=too-many-public-methods
     """ Tests API functionality """
-    maxDiff = None
-
     @classmethod
     def setUpTestData(cls):
         cls.client = APIClient()
@@ -247,6 +245,8 @@ class APITests(APITestCase): #pylint: disable=too-many-public-methods
             'max_credits': None,
             'instructor_name': 'Akash Tyagi',
             'honors': False,
+            'current_enrollment': 40,
+            'max_enrollment': 50,
             'meetings': [
                 {
                     'id': '10',
@@ -296,6 +296,8 @@ class APITests(APITestCase): #pylint: disable=too-many-public-methods
                 'max_credits': None,
                 'instructor_name': 'Akash Tyagi',
                 'honors': False,
+                'current_enrollment': 40,
+                'max_enrollment': 50,
                 'meetings': [
                     {
                         'id': '10',
@@ -326,6 +328,8 @@ class APITests(APITestCase): #pylint: disable=too-many-public-methods
                 'max_credits': None,
                 'instructor_name': 'John Moore',
                 'honors': False,
+                'current_enrollment': 40,
+                'max_enrollment': 50,
                 'meetings': [
                     {
                         'id': '20',
