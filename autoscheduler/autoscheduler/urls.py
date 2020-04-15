e08265a08324833a83a9ae8beb6f8df539e54aaf
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-04-15 01:14:53 -0500

    Added /scheduling/generate view & urls for it

--- autoscheduler/autoscheduler/urls.py
+++ autoscheduler/autoscheduler/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('scraper.urls')),
+    path('scheduler/', include('scheduler.urls')),
     path('', include('frontend.urls')),
 ]
