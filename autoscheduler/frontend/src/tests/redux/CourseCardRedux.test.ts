98c1a0df377d743de0891547e079d074e8655c68
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-04-24 10:10:58 -0500

    Added additional CourseCardRedux tests

--- autoscheduler/frontend/src/tests/redux/CourseCardRedux.test.ts
+++ autoscheduler/frontend/src/tests/redux/CourseCardRedux.test.ts
@@ -17,6 +17,66 @@ describe('Course Cards Redux', () => {
           course_num: '121',
           section_num: '500',
           min_credits: 3,
+          max_credits: 3,
+          current_enrollment: 0,
+          max_enrollment: 1,
+          instructor_name: 'Instructor Name',
+          web: false,
+          honors: false,
+          meetings: [
+            {
+              id: 11,
+              days: [true, false, false, false, false, false, false],
+              start_time: '08:00',
+              end_time: '08:50',
+              type: 'LEC',
+            },
+          ],
+        }];
+
+        const section = new Section({
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          courseNum: '121',
+          sectionNum: '500',
+          minCredits: 3,
+          maxCredits: 3,
+          currentEnrollment: 0,
+          maxEnrollment: 1,
+          instructor: new Instructor({ name: 'Instructor Name' }),
+        });
+
+        const expected = [
+          new Meeting({
+            id: 11,
+            building: '',
+            meetingDays: [true, false, false, false, false, false, false],
+            startTimeHours: 8,
+            startTimeMinutes: 0,
+            endTimeHours: 8,
+            endTimeMinutes: 50,
+            meetingType: MeetingType.LEC,
+            section,
+          }),
+        ];
+
+        // act
+        const output = parseMeetings(input);
+
+        // assert
+        expect(output).toEqual(expected);
+      });
+
+      test('with null maxCredits', () => {
+        // arrange
+        const input = [{
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          course_num: '121',
+          section_num: '500',
+          min_credits: 3,
           current_enrollment: 0,
           max_enrollment: 1,
           instructor_name: 'Instructor Name',
@@ -55,6 +115,66 @@ describe('Course Cards Redux', () => {
             startTimeMinutes: 0,
             endTimeHours: 8,
             endTimeMinutes: 50,
+            meetingType: MeetingType.LEC,
+            section,
+          }),
+        ];
+
+        // act
+        const output = parseMeetings(input);
+
+        // assert
+        expect(output).toEqual(expected);
+      });
+    });
+
+    describe('sets start/end time to 00:00', () => {
+      test('when start/end time are null', () => {
+        // arrange
+        const input = [{
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          course_num: '121',
+          section_num: '500',
+          min_credits: 3,
+          max_credits: 3,
+          current_enrollment: 0,
+          max_enrollment: 1,
+          instructor_name: 'Instructor Name',
+          web: false,
+          honors: false,
+          meetings: [
+            {
+              id: 11,
+              days: [true, false, false, false, false, false, false],
+              type: 'LEC',
+            },
+          ],
+        }];
+
+        const section = new Section({
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          courseNum: '121',
+          sectionNum: '500',
+          minCredits: 3,
+          maxCredits: 3,
+          currentEnrollment: 0,
+          maxEnrollment: 1,
+          instructor: new Instructor({ name: 'Instructor Name' }),
+        });
+
+        const expected = [
+          new Meeting({
+            id: 11,
+            building: '',
+            meetingDays: [true, false, false, false, false, false, false],
+            startTimeHours: 0,
+            startTimeMinutes: 0,
+            endTimeHours: 0,
+            endTimeMinutes: 0,
             meetingType: MeetingType.LEC,
             section,
           }),
