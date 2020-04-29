0470fc50950fa9eaeacb46e6051f01206c2887f6
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-04-29 14:47:58 -0500

    Added grades tests to CourseCardRedux tests

--- autoscheduler/frontend/src/tests/redux/CourseCardRedux.test.ts
+++ autoscheduler/frontend/src/tests/redux/CourseCardRedux.test.ts
@@ -2,6 +2,7 @@ import { parseSections } from '../../redux/actions/courseCards';
 import Meeting, { MeetingType } from '../../types/Meeting';
 import Section from '../../types/Section';
 import Instructor from '../../types/Instructor';
+import Grades from '../../types/Grades';
 
 // The input from the backend use snake_case, so disable camelcase errors for this file
 /* eslint-disable @typescript-eslint/camelcase */
@@ -10,6 +11,10 @@ describe('Course Cards Redux', () => {
     describe('parses correctly', () => {
       test('on a normal input', () => {
         // arrange
+        const grades = {
+          gpa: 4.0, A: 1, B: 0, C: 0, D: 0, F: 0, I: 0, S: 0, Q: 0, X: 0,
+        };
+
         const input = [{
           id: 1,
           crn: 1,
@@ -32,6 +37,7 @@ describe('Course Cards Redux', () => {
               type: 'LEC',
             },
           ],
+          grades,
         }];
 
         const section = new Section({
@@ -47,7 +53,7 @@ describe('Course Cards Redux', () => {
           honors: false,
           web: false,
           instructor: new Instructor({ name: 'Instructor Name' }),
-          grades: null,
+          grades: new Grades(grades),
         });
 
         const meetings = [new Meeting({
@@ -194,6 +200,70 @@ describe('Course Cards Redux', () => {
 
         // act
         const output = parseSections(input);
+
+        // assert
+        expect(output).toEqual(expected);
+      });
+    });
+
+    describe('Grades is null', () => {
+      test('When its given as null', () => {
+        // arrange
+        const input = [{
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          course_num: '121',
+          section_num: '500',
+          min_credits: 3,
+          max_credits: 3,
+          current_enrollment: 0,
+          max_enrollment: 1,
+          instructor_name: 'Instructor Name',
+          web: false,
+          honors: false,
+          meetings: [
+            {
+              id: 11,
+              days: [true, false, false, false, false, false, false],
+              start_time: '08:00',
+              end_time: '08:50',
+              type: 'LEC',
+            },
+          ],
+          grades: null as any,
+        }];
+
+        const section = new Section({
+          id: 1,
+          crn: 1,
+          subject: 'CSCE',
+          courseNum: '121',
+          sectionNum: '500',
+          minCredits: 3,
+          maxCredits: 3,
+          currentEnrollment: 0,
+          maxEnrollment: 1,
+          instructor: new Instructor({ name: 'Instructor Name' }),
+          grades: null as any,
+        });
+
+        const expected = [
+          new Meeting({
+            id: 11,
+            building: '',
+            meetingDays: [true, false, false, false, false, false, false],
+            startTimeHours: 8,
+            startTimeMinutes: 0,
+            endTimeHours: 8,
+            endTimeMinutes: 50,
+            meetingType: MeetingType.LEC,
+            section,
+          }),
+        ];
+
+        // act
+        const output = parseMeetings(input);
 
         // assert
         expect(output).toEqual(expected);
