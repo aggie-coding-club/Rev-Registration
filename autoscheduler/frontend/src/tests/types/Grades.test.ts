46a8d3d6853c593f6fea024877751a0c97071e8a
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-05-04 12:02:42 -0500

    Added null checks to Grades type and tests for it

---
+++ autoscheduler/frontend/src/tests/types/Grades.test.ts
@@ -0,0 +1,29 @@
+import Grades from '../../types/Grades';
+import { Indexable, fetchMock } from '../util';
+
+const correctArgs: Indexable = {
+  gpa: 0.0, A: 0, B: 0, C: 0, D: 0, F: 0, I: 0, S: 0, Q: 0, X: 0,
+};
+
+const createGrades = jest.fn((args) => new Grades(fetchMock(args)));
+
+describe('Grades type', () => {
+  describe('throws an error', () => {
+    const nonNullableProps = ['gpa', 'A', 'B', 'C', 'D', 'F', 'I', 'S', 'Q', 'X'];
+    test.each(nonNullableProps)('when any of the properties are null %s', (prop) => {
+      // arrange
+      const badArgs = { ...correctArgs };
+      badArgs[prop] = null;
+
+      // act/assert
+      expect(() => createGrades(badArgs)).toThrow();
+    });
+  });
+
+  describe('does not throw an error', () => {
+    test('when all of the props are not-null', () => {
+      // arrange/act/assert
+      expect(() => createGrades(correctArgs)).not.toThrow();
+    });
+  });
+});
--- autoscheduler/frontend/src/types/Grades.ts
+++ autoscheduler/frontend/src/types/Grades.ts
@@ -23,6 +23,18 @@ export default class Grades {
   Q: number;
   X: number;
   constructor(init: GradesInit) {
+    // None of the properties can be null
+    if (init.gpa == null) { throw Error(`Grades.gpa is invalid: ${init.gpa}`); }
+    if (init.A == null) { throw Error(`Grades.A is invalid: ${init.A}`); }
+    if (init.B == null) { throw Error(`Grades.B is invalid: ${init.B}`); }
+    if (init.C == null) { throw Error(`Grades.C is invalid: ${init.C}`); }
+    if (init.D == null) { throw Error(`Grades.D is invalid: ${init.D}`); }
+    if (init.F == null) { throw Error(`Grades.F is invalid: ${init.F}`); }
+    if (init.I == null) { throw Error(`Grades.I is invalid: ${init.I}`); }
+    if (init.S == null) { throw Error(`Grades.S is invalid: ${init.S}`); }
+    if (init.Q == null) { throw Error(`Grades.Q is invalid: ${init.Q}`); }
+    if (init.X == null) { throw Error(`Grades.X is invalid: ${init.X}`); }
+
     Object.assign(this, init);
   }
 }
