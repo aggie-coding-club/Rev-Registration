65643fac49031f680672e506b988d027a3dde66f
Ryan Conn <rconn478@gmail.com>
2020-07-11 18:39:09 -0500

    Created API for course sessions

--- autoscheduler/user_sessions/views.py
+++ autoscheduler/user_sessions/views.py
@@ -1,18 +1,21 @@
+import json
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser
 
 @api_view(['GET'])
 def get_last_term(request):
-    """ View that returns JSON containing last term for the current user's session.
-        Used by landing page to determine whether to redirect to schedules. """
-    term = request.session.setdefault('term', '')
-    response = {'term': term}
+    """ API endpoint that returns JSON containing last term for the user's session. """
+    term = request.session.get('term')
+    response = {}
+    if term:
+        response['term'] = term
     return Response(response)
 
 @api_view(['PUT'])
 def set_last_term(request):
-    """ View that sets term for the current session. Called when a term is selected,
-        or when the title bar is clicked to unset last term.
+    """ API endpoint that sets term for the current session. Called when a term is
+        selected, or when the title bar is clicked to unset last term.
     """
     term = request.query_params.get('term')
     # empty string term is valid (used to unset term), so explicitly check for None
@@ -20,3 +23,39 @@ def set_last_term(request):
         return Response(status=400)
     request.session['term'] = term
     return Response()
+
+@api_view(['PUT'])
+@parser_classes([JSONParser])
+def save_courses(request):
+    """ API endpoint that saves course cards for user's current term. """
+    try:
+        courses = json.loads(request.body.decode())
+    except (UnicodeError, json.JSONDecodeError):
+        return Response(status=400)
+
+    # Attempt to get user's session
+    session = request.session
+    term = session.get('term')
+    if not term:
+        return Response('No term set for user', status=500)
+
+    term_data = session.setdefault(term, {})
+    term_data['courses'] = courses
+    session.modified = True
+
+    return Response()
+
+@api_view(['GET'])
+def get_saved_courses(request):
+    """ API endpoint that retrieves saved courses for the requested term. """
+    term = request.query_params.get('term')
+    if not term:
+        return Response(status=400)
+
+    session = request.session
+    response = {}
+    if term:
+        courses = session.get(term, {}).get('courses')
+        if courses:
+            response = courses
+    return Response(response)
