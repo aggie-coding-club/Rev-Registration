e7d6985470f95b0032f5f73b927cb9cdf94bb884
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2020-07-20 13:16:21 -0500

    Created API route to retrieve full name of user

--- autoscheduler/user_sessions/urls.py
+++ autoscheduler/user_sessions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from user_sessions.views import get_last_term, set_last_term
+from user_sessions.views import get_last_term, set_last_term, get_full_name
 
 urlpatterns = [
     path('get_last_term', get_last_term),
     path('set_last_term', set_last_term),
+    path('get_full_name', get_full_name),
 ]
--- autoscheduler/user_sessions/views.py
+++ autoscheduler/user_sessions/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
 
 @api_view(['GET'])
 def get_last_term(request):
@@ -20,3 +21,14 @@ def set_last_term(request):
         return Response(status=400)
     request.session['term'] = term
     return Response()
+
+@api_view(['GET'])
+def get_full_name(request):
+    """ View that retrieves the first and last name seperated by a space for the user
+    of the current session """
+    user_id = request.session.get('_auth_user_id')
+    if user_id is None:
+        return Response(status=400)
+    user = User.objects.get(pk=user_id)
+    response = {user.get_full_name()}
+    return Response(response)
