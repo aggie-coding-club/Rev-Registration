05bc9212432404d4c31cc0f17e485d7de4b1ae5d
Adel Hassan <sean.dashals@gmail.com>
2020-07-25 12:40:12 -0500

    Simplified logic

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/ExpandedCourseCard.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/ExpandedCourseCard.tsx
@@ -115,7 +115,7 @@ const ExpandedCourseCard: React.FC<ExpandedCourseCardProps> = ({
           value={course}
           multiple={false}
           filterOptions={(): any[] => options} // Options are not filtered
-          getOptionSelected={(option): boolean => option === options[0]}
+          getOptionSelected={(): boolean => true}
           onClose={(): void => {
             if (!options.find((val) => val === inputValue)) setInputValue('');
           }}
