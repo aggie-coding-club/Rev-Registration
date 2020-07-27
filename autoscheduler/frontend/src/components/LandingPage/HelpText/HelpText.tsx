c3a318ca5e4fc7311093354d72a2c3cea85a6921
Adel Hassan <sean.dashals@gmail.com>
2020-07-26 19:01:41 -0500

    Added default route to catch 404s

--- autoscheduler/frontend/src/components/LandingPage/HelpText/HelpText.tsx
+++ autoscheduler/frontend/src/components/LandingPage/HelpText/HelpText.tsx
@@ -1,41 +1,22 @@
 import * as React from 'react';
-import {
-  Paper, Typography, Box,
-} from '@material-ui/core';
-import * as styles from './HelpText.css';
+import LargeTextCard from '../../LargeTextCard/LargeTextCard';
 
 const HelpText: React.SFC = () => (
-  <div className={styles.container}>
-    <Paper style={{ width: '55%', maxWidth: '800px' }}>
-      <Typography
-        variant="h3"
-        align="center"
-      >
-        <Box padding={2} paddingTop={2} paddingBottom={0}>
-          Guidelines
-        </Box>
-      </Typography>
-      <Box padding={2} paddingTop={1} paddingBottom={4}>
-        <Typography
-          variant="body1"
-          align="center"
-        >
-            Somebody once told me the world is gonna roll me
-            I ain&apos;t the sharpest tool in the shed
-            She was looking kind of dumb with her finger and her thumb
-            In the shape of an &quot;L&quot; on her forehead
-            Well the years start coming and they don&apos;t stop coming
-            Fed to the rules and I hit the ground running
-            Somebody once told me the world is gonna roll me
-            I ain&apos;t the sharpest tool in the shed
-            She was looking kind of dumb with her finger and her thumb
-            In the shape of an &quot;L&quot; on her forehead
-            Well the years start coming and they don&apos;t stop coming
-            Fed to the rules and I hit the ground running
-        </Typography>
-      </Box>
-    </Paper>
-  </div>
+  <LargeTextCard
+    title="Guidelines"
+    body="Somebody once told me the world is gonna roll me
+    I ain&apos;t the sharpest tool in the shed
+    She was looking kind of dumb with her finger and her thumb
+    In the shape of an &quot;L&quot; on her forehead
+    Well the years start coming and they don&apos;t stop coming
+    Fed to the rules and I hit the ground running
+    Somebody once told me the world is gonna roll me
+    I ain&apos;t the sharpest tool in the shed
+    She was looking kind of dumb with her finger and her thumb
+    In the shape of an &quot;L&quot; on her forehead
+    Well the years start coming and they don&apos;t stop coming
+    Fed to the rules and I hit the ground running"
+  />
 );
 
 export default HelpText;
