452da9f7fddf05762dd94e6685dac120b74efff4
Adel Hassan <sean.dashals@gmail.com>
2020-08-07 18:05:41 -0500

    Can now drag upwards

--- autoscheduler/frontend/src/redux/actions/selectedAvailability.ts
+++ autoscheduler/frontend/src/redux/actions/selectedAvailability.ts
@@ -33,11 +33,27 @@ export function mergeThenSelectAvailability(availability: AvailabilityArgs):
 ThunkAction<SelectedAvailabilityAction, RootState, void, SelectedAvailabilityAction> {
   return (dispatch, getState): SelectedAvailabilityAction => {
     const mergedAv = getState().availability.find((av) => !time1OnlyMismatch(av, availability));
+    const mergedStart = mergedAv.startTimeHours * 60 + mergedAv.startTimeMinutes;
+    const mergedEnd = mergedAv.endTimeHours * 60 + mergedAv.endTimeMinutes;
+    let time1;
+    let time2;
+    // if the newly merged availability preserved time1, try to keep it when
+    // adding the selected availability
+    if (availability.time1 === mergedStart) {
+      time1 = availability.time1;
+      time2 = mergedEnd;
+    } else if (availability.time1 === mergedEnd) {
+      time1 = availability.time1;
+      time2 = mergedStart;
+    } else {
+      time1 = mergedStart;
+      time2 = mergedEnd;
+    }
 
     return dispatch(addSelectedAvailability({
       ...mergedAv,
-      time1: mergedAv.startTimeHours * 60 + mergedAv.startTimeMinutes,
-      time2: mergedAv.endTimeHours * 60 + mergedAv.endTimeMinutes,
+      time1,
+      time2,
     }));
   };
 }
