106891c91332384580d33a52d2bca6c45cd1dbb5
Ryan Conn <rconn478@gmail.com>
2020-08-23 15:00:01 -0500

    throttle uses intervals instead of timeouts
    * If you're constantly updating cards, changes are now periodically saved

--- autoscheduler/frontend/src/utils/throttle.ts
+++ autoscheduler/frontend/src/utils/throttle.ts
@@ -6,32 +6,48 @@
  * @param runOtherIDs Whether to immediately run pending callbacks for IDs other than specified id
  */
 const throttle = ((): (id: string, callback: any, delay: number, runOtherIDs: boolean) => void => {
-  const timeouts = new Map<string, number>();
+  const intervals = new Map<string, number>();
+  const intervalTimes = new Map<string, number>();
   const functions = new Map<string, any>();
 
   return (id: string, callback: any, delay: number, runOtherIDs = false): void => {
-    // Remove old timeout + event listener
-    clearTimeout(timeouts.get(id));
-    window.removeEventListener('beforeunload', functions.get(id));
-    timeouts.delete(id);
-    functions.delete(id);
+    // Update interval frequency if needed
+    if (delay !== intervalTimes.get(id)) {
+      // Remove old callback, since the new one has already run
+      functions.delete(id);
 
-    // If runOtherIDs is specified, clear timeouts for each other ID
+      // Update interval with new delay
+      intervalTimes.set(id, delay);
+      clearInterval(intervals.get(id));
+      setInterval(() => {
+        // At the end of each interval, run callback if it's been updated since the last run,
+        // then remove the event listener as it's no longer needed
+        const toRun = functions.get(id);
+        if (typeof toRun === 'function') toRun();
+        functions.delete(id);
+        window.removeEventListener('beforeunload', functions.get(id));
+      }, delay);
+    }
+
+    // If runOtherIDs is specified, run and clear timeouts for each other ID
     if (runOtherIDs) {
       functions.forEach((fn, key) => {
-        clearTimeout(timeouts.get(key));
-        timeouts.delete(key);
-        fn();
-        functions.delete(key);
+        if (key !== id) {
+          clearInterval(intervals.get(key));
+          intervals.delete(key);
+          intervalTimes.delete(key);
+          functions.delete(key);
+          window.removeEventListener('beforeunload', fn);
+          fn();
+        }
       });
     }
 
-    // Create new timeout + event listener
-    const timeoutID = window.setTimeout(callback, delay);
-    timeouts.set(id, timeoutID);
-
-    window.addEventListener('beforeunload', callback);
+    // Update function to run when interval completes
     functions.set(id, callback);
+
+    // Update beforeunload event listener so callback is fired before close/refresh
+    window.addEventListener('beforeunload', callback);
   };
 })();
 
