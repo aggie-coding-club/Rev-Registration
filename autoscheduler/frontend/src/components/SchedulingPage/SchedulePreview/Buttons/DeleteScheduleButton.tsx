7c95f68e44066c8c85a412773453d7c5dbb2a4da
Ryan Conn <rconn478@gmail.com>
2020-08-29 17:47:39 -0500

    Created dialog for deleting a saved schedule

--- autoscheduler/frontend/src/components/SchedulingPage/SchedulePreview/Buttons/DeleteScheduleButton.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/SchedulePreview/Buttons/DeleteScheduleButton.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
-import { IconButton, Tooltip } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, IconButton, Tooltip,
+} from '@material-ui/core';
 import RemoveIcon from '@material-ui/icons/Delete';
 import { removeSchedule, unsaveSchedule } from '../../../../redux/actions/schedules';
+import { RootState } from '../../../../redux/reducer';
+import Meeting from '../../../../types/Meeting';
 
 interface DeleteScheduleButtonProps {
   index: number;
@@ -11,19 +15,79 @@ interface DeleteScheduleButtonProps {
 const DeleteScheduleButton: React.FC<DeleteScheduleButtonProps> = ({ index }) => {
   const dispatch = useDispatch();
 
-  const handleClick = (event: React.MouseEvent<HTMLElement>): void => {
-    event.stopPropagation();
+  const schedule = useSelector<RootState, Meeting[]>((state) => (
+    state.schedules.allSchedules[index]
+  ));
+  const savedSchedules = useSelector<RootState, Meeting[][]>((state) => (
+    state.schedules.savedSchedules
+  ));
 
+  const saved = savedSchedules.includes(schedule);
+
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+
+  const deleteSchedule = (): void => {
     dispatch(unsaveSchedule(index));
     dispatch(removeSchedule(index));
   };
 
+  const handleConfirmDelete = (): void => {
+    deleteSchedule();
+    setDialogOpen(false);
+  };
+
+  const handleDeleteButtonClick = (): void => {
+    // Show dialog confirmation if schedule is saved, otherwise just delete it
+    if (saved) setDialogOpen(true);
+    else deleteSchedule();
+  };
+
+  const handleDialogClick = (e: React.MouseEvent<HTMLElement>): void => {
+    // Prevent clicking through dialog
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleDialogClose = (): void => {
+    setDialogOpen(false);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLElement>): void => {
+    // Allow deleting by pressing enter instead of clicking the confirm button
+    if (dialogOpen && e.key === 'Enter') handleConfirmDelete();
+  };
+
   return (
-    <Tooltip title="Delete" placement="top">
-      <IconButton size="small" onClick={handleClick} data-testid="delete-schedule">
-        <RemoveIcon fontSize="inherit" />
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title="Delete" placement="top">
+        <IconButton size="small" onClick={handleDeleteButtonClick} data-testid="delete-schedule">
+          <RemoveIcon fontSize="inherit" />
+        </IconButton>
+      </Tooltip>
+      <Dialog
+        open={dialogOpen}
+        onClick={handleDialogClick}
+        onClose={handleDialogClose}
+        onKeyPress={handleKeyPress}
+      >
+        <DialogTitle>
+          Delete Saved Schedule
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {`This schedule is saved. Are you sure you want to delete schedule ${index + 1}?`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button color="secondary" variant="contained" onClick={handleDialogClose}>
+            Cancel
+          </Button>
+          <Button startIcon={<RemoveIcon />} color="primary" variant="contained" onClick={handleConfirmDelete}>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
