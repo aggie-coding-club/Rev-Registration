cdfbc86becebb8e2cd4fed43abb0fb113f2b4acd
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-08-30 12:38:40 -0500

    Moved entrypoint to docker/scrape-courses folder

--- docker/scrape-courses/entrypoint.sh
+++ docker/scrape-courses/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Connect to the thing
 export SETTINGS_MODE=proxy
-#./cloud_sql_proxy -instances=$GCP_INSTANCE_NAME=tcp:3306 &
 # Need to dynamically retrieve the current term?
 python3 manage.py scrape_courses -t 202031
