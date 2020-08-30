5c44053ffccce055dc97a968ab15b68f82ba9b32
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-08-30 12:43:44 -0500

    Updated docker settings file for schedule

--- docker/scrape-courses/entrypoint.sh
+++ docker/scrape-courses/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export SETTINGS_MODE=proxy
+export SETTINGS_MODE=schedule
 # Need to dynamically retrieve the current term?
-python3 manage.py scrape_courses -t 202031
+python3 manage.py scrape_courses -t 202031 --settings=autoscheduler.settings.docker
