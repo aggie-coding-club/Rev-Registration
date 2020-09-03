ad2980d750500dd25a08c494e9cbc1d8b76145b1
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2020-09-03 15:49:22 -0500

    Fixed the url redirect checking tests

--- autoscheduler/frontend/src/tests/ui/NavBar.test.tsx
+++ autoscheduler/frontend/src/tests/ui/NavBar.test.tsx
@@ -1,10 +1,9 @@
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
-// THESE TESTS ARE BROKEN. DO NOT TRUST/RELY ON THEM
 
 enableFetchMocks();
 /* eslint-disable import/first */ // enableFetchMocks must be called before others are imported
 import {
-  render, fireEvent, waitFor, act,
+  render, fireEvent, act,
 } from '@testing-library/react';
 import * as React from 'react';
 import { createStore } from 'redux';
@@ -12,11 +11,8 @@ import { Provider } from 'react-redux';
 import NavBar from '../../components/NavBar';
 import autoSchedulerReducer from '../../redux/reducer';
 
-// Mocks navigate, so we can assert that it redirected to the correct url for Redirects to /schedule
-// This must be outside of all describes in order to function correctly
-jest.mock('@reach/router', () => ({
-  navigate: jest.fn(),
-}));
+// Mocks window.open so it is possible to check if it is redirecting to the correct url
+window.open = jest.fn();
 
 // Mocks successful fetch call to sessions/get_full_name
 const mockSuccessfulGetNameAPI = (): void => {
@@ -39,10 +35,9 @@ describe('login button', () => {
 
   test('appears when user is logged out', async () => {
     // arrange
-    // THIS TEST WORKS
     mockFailedGetNameAPI();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     const { findByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -58,10 +53,9 @@ describe('login button', () => {
 
   test('does not appear when user is logged in', async () => {
     // arrange
-    // TEST WORKS
     mockSuccessfulGetNameAPI();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     const { findByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -77,12 +71,9 @@ describe('login button', () => {
 
   test('Redirects to /login/google-oauth2/ when clicked', async () => {
     // arrange
-    // DOESN'T WORK! ALWAYS PASSESS EVEN WHEN REPLACED WITH .not.toHaveBeenCalled
-    // OR WRONG PARAMETERS. Don't know why.
     mockFailedGetNameAPI();
-    window.open = jest.fn();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     const { findByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -94,7 +85,7 @@ describe('login button', () => {
     act(() => { fireEvent.click(loginButton); });
 
     // assert
-    waitFor(() => expect(window.open).toHaveBeenCalledWith('/login/google-oauth2/', '_self'));
+    expect(window.open).toHaveBeenCalledWith('/login/google-oauth2/', '_self');
   });
 });
 
@@ -105,10 +96,9 @@ describe('logout button', () => {
 
   test('appears when user is logged in', async () => {
     // arrange
-    // THIS TEST WORKS
     mockSuccessfulGetNameAPI();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     const { findByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -123,13 +113,12 @@ describe('logout button', () => {
   });
 
   test('does not appear when user is logged out', async () => {
-    // TEST WORKS
     // arrange
     mockFailedGetNameAPI();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     // Used queryByRole because instead of findByRole like the rest because findByRole
-    // returns an error when nothing is found and query does not
+    // returns an error when nothing is found and queryByRole does not
     const { queryByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -144,13 +133,10 @@ describe('logout button', () => {
   });
 
   test('Redirects to /sessions/logout when clicked', async () => {
-    // DOESN'T WORK! ALWAYS PASSESS EVEN WHEN REPLACED WITH .not.toHaveBeenCalled
-    // OR WRONG PARAMETERS. Don't know why.
     // arrange
     mockSuccessfulGetNameAPI();
-    window.open = jest.fn();
-    const store = createStore(autoSchedulerReducer);
 
+    const store = createStore(autoSchedulerReducer);
     const { findByRole } = render(
       <Provider store={store}>
         <NavBar />
@@ -162,6 +148,6 @@ describe('logout button', () => {
     act(() => { fireEvent.click(logoutButton); });
 
     // assert
-    waitFor(() => expect(window.open).toHaveBeenCalledWith('/sessions/logout', '_self'));
+    expect(window.open).toHaveBeenCalledWith('/sessions/logout', '_self');
   });
 });
