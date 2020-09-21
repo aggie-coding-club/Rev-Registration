e99647ba1756a9555bb9a8cc9982434f012fda02
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2020-09-20 19:18:34 -0500

    Removed debug print statements in retrieve_data_session.py

--- autoscheduler/user_sessions/utils/retrieve_data_session.py
+++ autoscheduler/user_sessions/utils/retrieve_data_session.py
@@ -13,33 +13,25 @@ def retrieve_data_session(request):
     # Get user
     user = request.user
     user_id = user.id
-    print("USER ID: " + str(user_id))
     # Session stuff
     try:
         if user_id is None:
-        # The User is not logged in. Let them use request.session
-            print("USER NOT LOGGED IN")
-            print("RETURNING request.session")
+        # The User is not logged in, uses request.session
             data_session_object = request.session
             yield request.session
         else:
-        # If user is logged in and model exists. Uses the session in the model
+        # If user is logged in and model exists, uses the session in the model
             user_to_data_session_model = UserToDataSession.objects.get(user_id=user_id)
-            print("FOUND EXISITNG OBJECT")
             data_session_key = user_to_data_session_model.session_key
-            print("DATA Session Key: " + data_session_key)
             data_session_object = SessionStore(session_key=data_session_key)
             yield data_session_object
     except UserToDataSession.DoesNotExist:
     # The user is logged in but the model doesn't exist.
     # Create the model before returning the corresponding data session
-        print("NO OBJECT ALREADY EXISTS")
-        print("CREATING DATA SESSION")
         # Create a session object
         data_session_object = SessionStore()
         data_session_object.create()
         data_session_key = data_session_object.session_key
-        print("DATA Session Key: " + data_session_key)
         # Create a user_to_data_session entry
         user_to_data_session_model = UserToDataSession()
         user_to_data_session_model.session_key = data_session_key
