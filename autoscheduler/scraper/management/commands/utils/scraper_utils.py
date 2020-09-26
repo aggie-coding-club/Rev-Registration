7ca583305798a17d75bb736aee70d4b2327b72f6
Gannon Prudhomme <gannonprudhomme@users.noreply.github.com>
2020-09-26 13:11:03 -0500

    GCP: Scrape courses on a schedule (#341)
    * Added docker stuff for scrape-courses
    
    * Granted running permissions to entrypoint for Docker
    
    * Added cloud-sql-proxy docker-compose image
    
    * Moved entrypoint to docker/scrape-courses folder
    
    * Updated docker settings file for schedule
    
    * Added 0.0.0.0 to cloud-sql-proxy container
    
    * Corrected backend-workflow
    
    * Fixed pylint duplicate-code warning
    
    Technically it's right, but I don't think it's necessary to fix it
    
    * Updated docker-compose comments
    
    * Added --recent flag to scrape_courses
    
    * Renamed the new scraper functions
    
    * Cleanup
    
    * Changed cloud-sql-proxy to use env var
    
    * Corrected which models get deleted for save_models
    
    * Added re-saving of grades
    
    * Updated per PR comments
    - Rearranged docker-compose
    - Change date formating for tests
    - Moved comment for grades_to_resave

--- autoscheduler/scraper/management/commands/utils/scraper_utils.py
+++ autoscheduler/scraper/management/commands/utils/scraper_utils.py
@@ -21,3 +21,39 @@ def get_all_terms(year: int = -1) -> List[str]:
 
     return [f"{year}{semester}{location}"
             for year, semester, location in product(years, semesters, locations)]
+
+def get_recent_semesters(now=datetime.now()) -> List[str]:
+    """ Calculates and returns which semester(s) we should be scraping for.
+        Returns semesters (just year + semester, no location), not terms. e.g. 20201
+    """
+
+    year = now.year
+
+    date_format = '%m/%d/%Y'
+
+    summer_fall_reg_start = datetime.strptime(f'04/01/{year}', date_format)
+    spring_reg_start = datetime.strptime(f'11/05/{year}', date_format)
+
+    # For the comments, use now.year = 2020
+    # Between [01/01/2020, 04/01/2020)
+    if now < summer_fall_reg_start:
+        return [f"{year}1"]
+
+    # Between [04/01/2020, 11/05/2020]
+    if summer_fall_reg_start <= now < spring_reg_start:
+        return [f"{year}2", f"{year}3"]
+
+    # Between [11/05/2020, 04/01/2021]
+    return [f"{year + 1}1"]
+
+def get_recent_terms(now=datetime.now()) -> List[str]:
+    """ Gets all of the most recent semesters + locations and combines them to get the
+        most recent terms to scrape.
+    """
+
+    recent_semesters = get_recent_semesters(now)
+
+    locations = range(1, 4)
+
+    return [f"{year_semester}{location}"
+            for year_semester, location in product(recent_semesters, locations)]
