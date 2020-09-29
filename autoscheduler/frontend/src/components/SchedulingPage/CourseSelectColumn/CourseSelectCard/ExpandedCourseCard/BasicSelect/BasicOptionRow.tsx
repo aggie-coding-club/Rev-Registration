b7d53bc4466578dec3cc09741c15e0a825a0f996
Gannon Prudhomme <gannonprudhomme@gmail.com>
2020-09-29 01:02:38 -0500

    Changed default in BasicOptionRow

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/BasicSelect/BasicOptionRow.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/BasicSelect/BasicOptionRow.tsx
@@ -16,7 +16,7 @@ interface BasicOptionRowProps {
  * option selected by this row, formatted as it is found in the Redux course cards
  */
 const BasicOptionRow: React.FC<BasicOptionRowProps> = ({ id, value }) => {
-  const option = useSelector<RootState, string>((state) => state.courseCards[id][value] || 'exclude');
+  const option = useSelector<RootState, string>((state) => state.courseCards[id][value] || 'no_preference');
   const dispatch = useDispatch();
 
   return (
