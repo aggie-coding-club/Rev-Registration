fed7541be76a98180ad4a503bbebd4153d05155c
Adel Hassan <sean.dashals@gmail.com>
2020-10-03 16:18:54 -0500

    Changed colors for course cards away from maroon

--- autoscheduler/frontend/src/components/SchedulingPage/Schedule/meetingColors.ts
+++ autoscheduler/frontend/src/components/SchedulingPage/Schedule/meetingColors.ts
@@ -2,8 +2,8 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/reducer';
 
 export const colors = [
-  '#500000', '#733333', '#966666', '#b99999',
-  '#871b1e', '#9a1d26', '#c2777d', '#9f494b', '#b76778',
+  '#d19642', '#ec4631', '#951c26', '#73203c',
+  '#227d9c', '#2d683e', '#c47652', '#612b0d', '#249889',
 ];
 
 export default function useMeetingColor(): Map<string, string> {
