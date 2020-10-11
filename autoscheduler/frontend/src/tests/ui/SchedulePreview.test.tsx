62b26eadfdaee42c61021dc85e31e6fdc43ab712
Ryan Conn <rconn478@gmail.com>
2020-10-11 15:06:01 -0500

    Rename schedules (#339)
    * Created schedule type
    
    * Updated schedules redux
    
    * Updated tests + components with new schedule redux
    
    * Created renameSchedule action
    
    * Created tests for renameSchedules, schedule name uniqueness
    
    * Created schedule name component
    
    * Updated tests to work with ScheduleName component
    
    * Linter fixes
    
    * Created tests for renaming schedules
    
    * Schedule name uses onKeyDown to detect escape
    
    * Created hook to update ref dimensions on resize
    
    * Fixed schedule name styling
    * Added extra padding for name
    * Name uses ellipsis overflow
    
    * Trim schedule names, duplicates start counting at 1
    
    * Display schedule name when deleting
    
    * Text field takes up full width when renaming
    
    * Schedules redux no longer deep copies schedules

--- autoscheduler/frontend/src/tests/ui/SchedulePreview.test.tsx
+++ autoscheduler/frontend/src/tests/ui/SchedulePreview.test.tsx
@@ -19,7 +19,7 @@ describe('SchedulePreview component', () => {
     test('when the user clicks on the second schedule', async () => {
       // arrange
       const store = createStore(autoSchedulerReducer);
-      const { findByText } = render(
+      const { findAllByLabelText } = render(
         <Provider store={store}>
           <SchedulePreview />
         </Provider>,
@@ -27,7 +27,8 @@ describe('SchedulePreview component', () => {
       store.dispatch(replaceSchedules([testSchedule1, testSchedule2]));
 
       // act
-      fireEvent.click(await findByText('Schedule 2'));
+      const schedules = await findAllByLabelText('Schedule preview');
+      fireEvent.click(schedules[1]);
 
       // assert
       expect(store.getState().selectedSchedule).toBe(1);
@@ -166,7 +167,7 @@ describe('SchedulePreview component', () => {
 
       // assert
       await waitFor(() => (
-        expect(store.getState().schedules.savedSchedules).toContainEqual(testSchedule1)
+        expect(store.getState().schedules[0].saved).toBe(true)
       ));
     });
 
@@ -186,7 +187,7 @@ describe('SchedulePreview component', () => {
 
       // assert
       await waitFor(() => (
-        expect(store.getState().schedules.savedSchedules).toContainEqual(testSchedule2)
+        expect(store.getState().schedules[1].saved).toBe(true)
       ));
     });
   });
@@ -211,7 +212,7 @@ describe('SchedulePreview component', () => {
 
       // assert
       await waitFor(() => (
-        expect(store.getState().schedules.savedSchedules).toHaveLength(0)
+        expect(store.getState().schedules.filter((schedule) => schedule.saved)).toHaveLength(0)
       ));
     });
 
@@ -234,7 +235,7 @@ describe('SchedulePreview component', () => {
 
       // assert
       await waitFor(() => (
-        expect(store.getState().schedules.savedSchedules).toHaveLength(0)
+        expect(store.getState().schedules.filter((schedule) => schedule.saved)).toHaveLength(0)
       ));
     });
   });
@@ -255,9 +256,9 @@ describe('SchedulePreview component', () => {
       fireEvent.click(deleteScheduleButton);
 
       // assert
-      const schedules = store.getState().schedules.allSchedules;
+      const { schedules } = store.getState();
       expect(schedules).toHaveLength(1);
-      expect(schedules[0]).toEqual(testSchedule1);
+      expect(schedules[0].meetings).toEqual(testSchedule1);
     });
 
     test('when deleted from the dialog from a saved schedule', async () => {
@@ -281,10 +282,70 @@ describe('SchedulePreview component', () => {
       fireEvent.click(confirmDeleteButton);
 
       // assert
-      const { allSchedules, savedSchedules } = store.getState().schedules;
-      expect(savedSchedules).toHaveLength(0);
-      expect(allSchedules).toHaveLength(1);
-      expect(allSchedules[0]).toEqual(testSchedule1);
+      const { schedules } = store.getState();
+      expect(schedules).toHaveLength(1);
+      expect(schedules[0].saved).toBe(false);
+      expect(schedules[0].meetings).toEqual(testSchedule1);
+    });
+  });
+
+  describe('renames the correct schedule', () => {
+    test('when the first schedule is renamed', async () => {
+      // arrange
+      const newScheduleName = 'Test schedule';
+
+      const store = createStore(autoSchedulerReducer);
+      const { findByLabelText, findAllByLabelText } = render(
+        <Provider store={store}>
+          <SchedulePreview />
+        </Provider>,
+      );
+      store.dispatch(replaceSchedules([testSchedule1, testSchedule2]));
+
+      // act
+      // click button to rename schedule
+      const renameScheduleButton = (await findAllByLabelText('Rename schedule'))[0];
+      fireEvent.click(renameScheduleButton);
+
+      // set new name
+      const scheduleNameInput = (await findByLabelText('Schedule name'));
+      if (!(scheduleNameInput instanceof HTMLInputElement)) throw Error('Input element is not valid');
+      fireEvent.change(scheduleNameInput, { target: { value: newScheduleName } });
+
+      // confirm new name
+      fireEvent.click(renameScheduleButton);
+
+      // assert
+      expect(store.getState().schedules[0].name).toBe(newScheduleName);
+    });
+
+    test('when the second schedule is renamed', async () => {
+      // arrange
+      const newScheduleName = 'Cool classes for cool kids';
+
+      const store = createStore(autoSchedulerReducer);
+      const { findByLabelText, findAllByLabelText } = render(
+        <Provider store={store}>
+          <SchedulePreview />
+        </Provider>,
+      );
+      store.dispatch(replaceSchedules([testSchedule1, testSchedule2]));
+
+      // act
+      // click button to rename schedule
+      const renameScheduleButton = (await findAllByLabelText('Rename schedule'))[1];
+      fireEvent.click(renameScheduleButton);
+
+      // set new name
+      const scheduleNameInput = (await findByLabelText('Schedule name'));
+      if (!(scheduleNameInput instanceof HTMLInputElement)) throw Error('Input element is not valid');
+      fireEvent.change(scheduleNameInput, { target: { value: newScheduleName } });
+
+      // confirm new name
+      fireEvent.click(renameScheduleButton);
+
+      // assert
+      expect(store.getState().schedules[1].name).toBe(newScheduleName);
     });
   });
 });
