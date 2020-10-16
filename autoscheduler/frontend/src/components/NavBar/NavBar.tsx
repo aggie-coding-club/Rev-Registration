b22d1e7adec4ab6ff8ab3624de13ba0373eaa4da
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2020-10-16 14:48:45 -0500

    Readded the NavBar folder and all the files inside it
    Casing is correct this time though. Hopefully this fixes the issue and allows the remote to compile the frontend

---
+++ autoscheduler/frontend/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import {
+  AppBar, Toolbar, Typography, Button, makeStyles,
+} from '@material-ui/core';
+import { navigate } from '@reach/router';
+import LoginButton from './LoginButton';
+import appTheme from '../../theme';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 0,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  buttonText: {
+    textTransform: 'capitalize',
+  },
+}));
+
+const NavBar: React.SFC = () => {
+  const classes = useStyles(appTheme);
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        color="primary"
+      >
+        <Toolbar>
+          <div className={classes.title}>
+            <Button
+              color="inherit"
+              classes={{ label: classes.buttonText }}
+              onClick={(): void => {
+                // Navigate to the root when the title is clicked
+                navigate('/');
+              }}
+            >
+              <Typography variant="h6">
+                Rev Registration
+              </Typography>
+            </Button>
+          </div>
+          <LoginButton />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default NavBar;
