b16ff9810c79004d4a71d13627ff92240e1b5e34
fool1280 <anhnguyen@Anhs-MacBook-Pro.local>
2020-10-25 17:57:04 -0500

    update lint

--- autoscheduler/frontend/src/components/LandingPage/About/About.tsx
+++ autoscheduler/frontend/src/components/LandingPage/About/About.tsx
@@ -1,68 +1,85 @@
 import * as React from 'react';
-import { Link, Dialog, Typography, DialogContent, DialogTitle } from '@material-ui/core';
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
-import { withStyles } from "@material-ui/core/styles";
+import {
+  Link, Dialog, Typography, DialogContent, DialogTitle,
+} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { withStyles } from '@material-ui/core/styles';
 
 import * as about from './About.css';
 
 const About: React.FC = () => {
-      
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClickOpen = (): void => {
+    setOpen(true);
+  };
+  const handleClose = (): void => {
+    setOpen(false);
+  };
 
-    const WhiteTextTypography = withStyles({
-        root: {
-          color: "#FFFFFF"
-        }
-    })(Typography);
+  const WhiteTextTypography = withStyles({
+    root: {
+      color: '#FFFFFF',
+    },
+  })(Typography);
 
-    return( 
-        <div className={about.container}>
-            <Link
-                component="button"
-                variant="body2"
-                onClick={handleClickOpen}
-            >
+  return (
+    <div className={about.container}>
+      <Link
+        component="button"
+        variant="body2"
+        onClick={handleClickOpen}
+      >
                 About Rev Registration
-            </Link>
-            <Dialog
-                onClose={handleClose}
-                open={open}
-            >
-                <DialogTitle disableTypography className={about.title}>
-                    <WhiteTextTypography  variant="h6">About</WhiteTextTypography>
-                    <IconButton onClick={handleClose}>
-                        <CloseIcon style={{ color: 'white'}}/>
-                    </IconButton>
-                </DialogTitle>
-                <DialogContent dividers>
-                    <Typography gutterBottom>
-                        <b>About</b> 
-                        <p>Rev Registration is an open-source <a href="https://aggiecodingclub.com/">Aggie Coding Club</a> project
-                        led by Gannon Prudhomme and Ryan Conn.</p>
-                        <p>You can find all our codes on <a href="https://github.com/aggie-coding-club">Github</a>.</p>
-                    </Typography>
-                    <Typography gutterBottom>
-                        <b>Contributors</b>
-                    </Typography>
-                    <Typography gutterBottom>
-                        <b>Previous Contributors</b>
-                    </Typography>
-                    <Typography gutterBottom>
-                        <b>License</b>
-                        <p>If you have any questions or suggestions, shoot us an email at <a href = "mailto: revregistration@gmail.com">revregistration@gmail.com</a>.</p>
-                    </Typography>
-                </DialogContent>
-            </Dialog>
-        </div>
-    );
-}
+      </Link>
+      <Dialog
+        onClose={handleClose}
+        open={open}
+      >
+        <DialogTitle disableTypography className={about.title}>
+          <WhiteTextTypography variant="h6">About</WhiteTextTypography>
+          <IconButton onClick={handleClose}>
+            <CloseIcon style={{ color: 'white' }} />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            <b>About</b>
+            <p>
+Rev Registration is an open-source
+              {' '}
+              <a href="https://aggiecodingclub.com/">Aggie Coding Club</a>
+              {' '}
+project
+                        led by Gannon Prudhomme and Ryan Conn.
+
+            </p>
+            <p>
+You can find all our codes on
+              {' '}
+              <a href="https://github.com/aggie-coding-club">Github</a>
+.
+            </p>
+          </Typography>
+          <Typography gutterBottom>
+            <b>Contributors</b>
+          </Typography>
+          <Typography gutterBottom>
+            <b>Previous Contributors</b>
+          </Typography>
+          <Typography gutterBottom>
+            <b>License</b>
+            <p>
+If you have any questions or suggestions, shoot us an email at
+              <a href="mailto: revregistration@gmail.com">revregistration@gmail.com</a>
+.
+            </p>
+          </Typography>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
 
 export default About;
