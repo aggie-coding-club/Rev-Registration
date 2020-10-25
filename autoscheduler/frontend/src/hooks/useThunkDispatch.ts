133c9dadec15cb417a8527ea4775ddd1f19b4cb3
Adel Hassan <sean.dashals@gmail.com>
2020-10-25 00:08:19 -0500

    Added comment to useThunkDispatch

--- autoscheduler/frontend/src/hooks/useThunkDispatch.ts
+++ autoscheduler/frontend/src/hooks/useThunkDispatch.ts
@@ -3,6 +3,12 @@ import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { RootState } from '../redux/reducer';
 
+/**
+ * Casts the return type for Redux dispatches into ThunkDispatches. The key difference
+ * is that Thunk Dispatches are asynchronous, and they can be `.then`ed and `.catch`ed
+ *
+ * https://www.reddit.com/r/typescript/comments/c04mjt/how_to_type_reduxthunks_with_the_new_usedispatch/
+ */
 export default function useThunkDispatch(): ThunkDispatch<RootState, undefined, AnyAction> {
   return useDispatch();
 }
