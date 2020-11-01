c90c87538e63d609a8720b2fb9ec430ec4921dd8
ajayramsunder <ajayramsunder@tamu.edu>
2020-11-01 15:54:49 -0600

    Cleaner Code

--- autoscheduler/frontend/src/utils/sortMeetingFunction.tsx
+++ autoscheduler/frontend/src/utils/sortMeetingFunction.tsx
@@ -1,8 +1,5 @@
 import Meeting from '../types/Meeting';
 
-// sorts meeetings based on meeting type
-const sortMeeting = (a: Meeting, b: Meeting): number => {
-  const value = a.meetingType - b.meetingType;
-  return value;
-};
+/**  sorts meeetings based on meeting type */
+const sortMeeting = (a: Meeting, b: Meeting): number => a.meetingType - b.meetingType;
 export default sortMeeting;
