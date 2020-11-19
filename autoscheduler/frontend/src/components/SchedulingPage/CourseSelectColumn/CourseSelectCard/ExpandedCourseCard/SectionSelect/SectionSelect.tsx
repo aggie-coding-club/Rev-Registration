a21c49643f84d25a9842852a58156c8578775cfa
Troy Lee <troylee3006@gmail.com>
2020-11-18 22:22:54 -0600

    double-ripple

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/SectionSelect/SectionSelect.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/SectionSelect/SectionSelect.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { List, Typography, Checkbox } from '@material-ui/core';
-import ToggleButton from '@material-ui/lab/ToggleButton';
-
+import {
+  List, Typography, Checkbox, FormControlLabel, ButtonBase,
+} from '@material-ui/core';
+import { ToggleButton } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/styles';
 import { toggleSelectedAll } from '../../../../../../redux/actions/courseCards';
 import { SectionSelected } from '../../../../../../types/CourseCardOptions';
@@ -25,6 +26,7 @@ const SectionSelect: React.FC<SectionSelectProps> = ({ id }): JSX.Element => {
     rootToggleButton: {
       border: 'none',
       textAlign: 'left',
+      margin: '5px 0',
       padding: '0 10px 0 0',
       fontSize: '86%',
       fontWeight: 500,
@@ -94,6 +96,26 @@ const SectionSelect: React.FC<SectionSelectProps> = ({ id }): JSX.Element => {
   const list = makeList();
   const allSelected: boolean = countSelected === sections.length;
   const sectionSelectOptions = (
+    // <ButtonBase component="div" onClick={null}>
+    //   <FormControlLabel
+    //     control={(
+    //       <Checkbox
+    //         checked={allSelected}
+    //         value={(allSelected) ? 'allOn' : 'allOff'}
+    //         color="primary"
+    //         size="small"
+    //         disableRipple
+    //         classes={{ root: classes.rootCheckbox }}
+    //         onChange={(): void => { dispatch(toggleSelectedAll(id, !allSelected)); }}
+    //       />
+    //     )}
+    //     label={(
+    //       <Typography variant="button">
+    //         SELECT ALL
+    //       </Typography>
+    //     )}
+    //   />
+    // </ButtonBase>
     <div>
       <ToggleButton classes={{ root: classes.rootToggleButton }} value="select-all" aria-label="select all" onChange={(): void => { dispatch(toggleSelectedAll(id, !allSelected)); }}>
         <Checkbox
@@ -101,9 +123,10 @@ const SectionSelect: React.FC<SectionSelectProps> = ({ id }): JSX.Element => {
           value={(allSelected) ? 'allOn' : 'allOff'}
           color="primary"
           size="small"
+          disableRipple
           classes={{ root: classes.rootCheckbox }}
         />
-        SELECT ALL
+          SELECT ALL
       </ToggleButton>
     </div>
   );
