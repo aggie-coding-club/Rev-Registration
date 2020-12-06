d83b4aa99ec65ff61b39882e7b25c9c502dae2ae
Adel Hassan <sean.dashals@gmail.com>
2020-12-05 22:09:53 -0600

    Style fixes 1

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/CourseSelectCard.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/CourseSelectCard.tsx
@@ -25,14 +25,12 @@ interface CourseSelectCardProps {
 }
 
 const CourseSelectCard: React.FC<CourseSelectCardProps> = ({ id }) => {
-  const courseCardOptions = useSelector<RootState, CourseCardOptions>(
-    (state) => state.courseCards[id],
-  );
-
+  const dispatch = useDispatch();
   const term = useSelector<RootState, string>((state) => state.term);
   const collapsed = useSelector<RootState, boolean>((state) => state.courseCards[id].collapsed);
-  const dispatch = useDispatch();
-  const { course, customizationLevel, loading } = courseCardOptions;
+  const { course, customizationLevel, loading } = useSelector<RootState, CourseCardOptions>(
+    (state) => state.courseCards[id],
+  );
 
   const [options, setOptions] = React.useState([]);
   const [inputValue, setInputValue] = React.useState('');
@@ -41,11 +39,59 @@ const CourseSelectCard: React.FC<CourseSelectCardProps> = ({ id }) => {
     dispatch(updateCourseCard(id, { collapsed: !collapsed }));
   };
 
+  const header = (
+    <div
+      className={styles.header}
+      onClick={toggleCollapsed}
+      role="button"
+      tabIndex={0}
+      onKeyPress={toggleCollapsed}
+    >
+      <div
+        className={styles.headerGroup}
+        onClick={(evt): void => {
+          dispatch(removeCourseCard(id));
+          evt.stopPropagation();
+        }}
+        role="button"
+        tabIndex={0}
+        onKeyPress={(evt): void => {
+          dispatch(removeCourseCard(id));
+          evt.stopPropagation();
+        }}
+        aria-label="Remove"
+      >
+        <RemoveIcon />
+        {!collapsed && 'Remove'}
+      </div>
+      {collapsed && course}
+      <div className={styles.headerGroup}>
+        <CollapseIcon
+          classes={{ root: styles.rotatableIcon }}
+          style={{
+            transform: collapsed ? '' : 'rotate(180deg)',
+          }}
+          aria-label={collapsed ? 'Expand' : 'Collapse'}
+        />
+      </div>
+    </div>
+  );
+
   const contentRef = React.useRef<HTMLDivElement>(null);
+  /**
+   * Fixes the height of the sectionRows element.
+   *
+   * Before this function is called, sectionRows will expand to fit every single one of its
+   * sections. This function measures the height of the parent row and sets the height of
+   * sectionRows to the height of the row minus 182 (the height of the rest of the row)
+   */
   const fixHeight = React.useCallback((): void => {
+    // find the expandedRow that is the parent of this card
     let parentEl = contentRef.current?.parentElement;
     while (parentEl
       && !parentEl.classList.contains(parentStyles.expandedRow)) {
+      // if this card is in an expandedRowSmall, we'll need to re-calculate the height later,
+      // once the class has been changed to expandedRow and the overflow-y is hidden
       if (parentEl.classList.contains(parentStyles.expandedRowSmall)) {
         const observer = new MutationObserver((mutations) => {
           observer.disconnect();
@@ -57,14 +103,18 @@ const CourseSelectCard: React.FC<CourseSelectCardProps> = ({ id }) => {
         observer.observe(parentEl, { attributes: true });
         return;
       }
+      // if the current parentEl points to an element that is neither expandedRow nor
+      // expandedRowSmall, keep looking
       parentEl = parentEl.parentElement;
     }
     // not all cards are currently in expanded rows
     if (parentEl) {
+      // but for those that are, set the height in pixels so that it doesn't overflow
       const sectionRows = contentRef.current
         .getElementsByClassName(childStyles.sectionRows)[0] as HTMLElement;
       if (sectionRows) {
-        // 182 is epxerimentally measured and will need to be updated as card content changes
+        // 182 is experimentally measured from the total height of the card content minus
+        // the height of the sectionRows. Will need to be updated as card content changes
         sectionRows.style.height = `${parentEl.scrollHeight - 182}px`;
       }
     }
@@ -112,111 +162,81 @@ const CourseSelectCard: React.FC<CourseSelectCardProps> = ({ id }) => {
     );
   };
 
+  const collapsibleContent = (
+    <Collapse in={!collapsed} onEntered={fixHeight}>
+      <div className={styles.content} ref={contentRef} aria-hidden={collapsed}>
+        <Autocomplete
+          options={options}
+          size="small"
+          autoHighlight
+          autoSelect
+          disabled={loading}
+          inputValue={inputValue}
+          value={course}
+          multiple={false}
+          filterOptions={(): any[] => options} // Options are not filtered
+          getOptionSelected={(option): boolean => option === options[0]}
+          onClose={(): void => {
+            if (options.length === 0) setInputValue('');
+          }}
+          onChange={(_evt: object, val: string): void => {
+            dispatch(updateCourseCard(id, {
+              course: val,
+            }, term));
+          }}
+          onInputChange={(_evt: object, val: string, reason: string): void => {
+            setInputValue(val);
+            if (val === '') { // Skip empty inputs
+              setOptions([]); // Clear the autocomplete options so previous results don't show
+              return;
+            }
+
+            if (reason !== 'reset') getAutocomplete(val);
+          }}
+          renderInput={(params: any): JSX.Element => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <TextField {...params} label="Course" fullWidth autoFocus variant="outlined" />
+          )}
+          classes={{ root: styles.courseInput }}
+        />
+        <FormLabel component="label" style={{ marginTop: 16 }} focused={false}>
+          Customization Level:
+        </FormLabel>
+        <ButtonGroup className={styles.customizationButtons}>
+          <Button
+            className={styles.noElevation}
+            color="primary"
+            variant={customizationLevel === CustomizationLevel.BASIC ? 'contained' : 'outlined'}
+            onClick={(): void => {
+              dispatch(updateCourseCard(id, {
+                customizationLevel: CustomizationLevel.BASIC,
+              }));
+            }}
+          >
+            Basic
+          </Button>
+          <Button
+            className={styles.noElevation}
+            color="primary"
+            variant={customizationLevel === CustomizationLevel.SECTION ? 'contained' : 'outlined'}
+            onClick={(): void => {
+              dispatch(updateCourseCard(id, {
+                customizationLevel: CustomizationLevel.SECTION,
+              }));
+            }}
+          >
+            Section
+          </Button>
+        </ButtonGroup>
+        {getCustomizationContent()}
+      </div>
+    </Collapse>
+  );
+
   return (
     <Card className={styles.card}>
-      <div
-        className={styles.header}
-        onClick={toggleCollapsed}
-        role="button"
-        tabIndex={0}
-        onKeyPress={toggleCollapsed}
-      >
-        <div
-          className={styles.headerGroup}
-          onClick={(evt): void => {
-            dispatch(removeCourseCard(id));
-            evt.stopPropagation();
-          }}
-          role="button"
-          tabIndex={0}
-          onKeyPress={(evt): void => {
-            dispatch(removeCourseCard(id));
-            evt.stopPropagation();
-          }}
-          aria-label="Remove"
-        >
-          <RemoveIcon />
-          {!collapsed && 'Remove'}
-        </div>
-        {collapsed && course}
-        <div className={styles.headerGroup}>
-          <CollapseIcon
-            classes={{ root: styles.rotatableIcon }}
-            style={{
-              transform: collapsed ? '' : 'rotate(180deg)',
-            }}
-            aria-label={collapsed ? 'Expand' : 'Collapse'}
-          />
-        </div>
-      </div>
-      <Collapse in={!collapsed} onEntered={fixHeight}>
-        <div className={styles.content} ref={contentRef} aria-hidden={collapsed}>
-          <Autocomplete
-            options={options}
-            size="small"
-            autoHighlight
-            autoSelect
-            disabled={loading}
-            inputValue={inputValue}
-            value={course}
-            multiple={false}
-            filterOptions={(): any[] => options} // Options are not filtered
-            getOptionSelected={(option): boolean => option === options[0]}
-            onClose={(): void => {
-              if (options.length === 0) setInputValue('');
-            }}
-            onChange={(_evt: object, val: string): void => {
-              dispatch(updateCourseCard(id, {
-                course: val,
-              }, term));
-            }}
-            onInputChange={(_evt: object, val: string, reason: string): void => {
-              setInputValue(val);
-              if (val === '') { // Skip empty inputs
-                setOptions([]); // Clear the autocomplete options so previous results don't show
-                return;
-              }
-
-              if (reason !== 'reset') getAutocomplete(val);
-            }}
-            renderInput={(params: any): JSX.Element => (
-            // eslint-disable-next-line react/jsx-props-no-spreading
-              <TextField {...params} label="Course" fullWidth autoFocus variant="outlined" />
-            )}
-            classes={{ root: styles.courseInput }}
-          />
-          <FormLabel component="label" style={{ marginTop: 16 }} focused={false}>
-            Customization Level:
-          </FormLabel>
-          <ButtonGroup className={styles.customizationButtons}>
-            <Button
-              className={styles.noElevation}
-              color="primary"
-              variant={customizationLevel === CustomizationLevel.BASIC ? 'contained' : 'outlined'}
-              onClick={(): void => {
-                dispatch(updateCourseCard(id, {
-                  customizationLevel: CustomizationLevel.BASIC,
-                }));
-              }}
-            >
-              Basic
-            </Button>
-            <Button
-              className={styles.noElevation}
-              color="primary"
-              variant={customizationLevel === CustomizationLevel.SECTION ? 'contained' : 'outlined'}
-              onClick={(): void => {
-                dispatch(updateCourseCard(id, {
-                  customizationLevel: CustomizationLevel.SECTION,
-                }));
-              }}
-            >
-              Section
-            </Button>
-          </ButtonGroup>
-          {getCustomizationContent()}
-        </div>
-      </Collapse>
+      {header}
+      {collapsibleContent}
     </Card>
   );
 };
