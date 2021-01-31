bd02e7754c6470fab7513aaae8365fae4b9a3d53
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2021-01-30 20:56:19 -0600

    Changed return for when a course is selected
    Rewrote code for contents of the return statement (formerly statements) when a course is selected. The content is now decided by variables inside the statement instead of choosing between two return options based on whether there are filter options

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/BasicSelect/BasicSelect.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/BasicSelect/BasicSelect.tsx
@@ -24,6 +24,32 @@ const BasicSelect: React.FC<BasicSelectProps> = ({ id }) => {
     (state) => state.courseCards[id].hasAsynchronous || false,
   );
 
+  let filterOptions;
+  let noOptionsMessage;
+  // if filter options available generate the dropdowns for them
+  if (hasHonors || hasRemote || hasAsynchronous) {
+    filterOptions = (
+      <>
+        {hasHonors
+          ? <BasicOptionRow id={id} value="honors" label="Honors" />
+          : null}
+        {hasRemote
+          ? <BasicOptionRow id={id} value="remote" label="Remote" />
+          : null}
+        {hasAsynchronous
+          ? <BasicOptionRow id={id} value="asynchronous" label="No Meeting Times" />
+          : null}
+      </>
+    );
+  } else {
+  // no filter options available. Generate message to alert user
+    noOptionsMessage = (
+      <Typography className={styles.placeholderText} color="textSecondary">
+    There are no honors or remote sections for this class
+      </Typography>
+    );
+  }
+
   // shows placeholder text if no course is selected
   if (!course) {
     return (
@@ -33,42 +59,20 @@ const BasicSelect: React.FC<BasicSelectProps> = ({ id }) => {
     );
   }
 
-  // show placeholder message if there are no special sections to filter
-  if (!hasHonors && !hasRemote && !hasAsynchronous) {
-    return (
-      <>
-        <FormLabel>Options</FormLabel>
-        <table className={styles.tableContainer}>
-          <tbody>
-            <BasicCheckbox id={id} value="includeFull" label="Include Full Sections" />
-          </tbody>
-        </table>
-        <Typography className={styles.placeholderText} color="textSecondary">
-        There are no honors or remote sections for this class
-        </Typography>
-      </>
-    );
-  }
-
+  // shows include full checkbox and either available options or the no options message
   return (
     <>
       <FormLabel>Options</FormLabel>
       <table className={styles.tableContainer}>
         <tbody>
           <BasicCheckbox id={id} value="includeFull" label="Include Full Sections" />
-          {hasHonors
-            ? <BasicOptionRow id={id} value="honors" label="Honors" />
-            : null}
-          {hasRemote
-            ? <BasicOptionRow id={id} value="remote" label="Remote" />
-            : null}
-          {hasAsynchronous
-            ? <BasicOptionRow id={id} value="asynchronous" label="No Meeting Times" />
-            : null}
+          {filterOptions}
         </tbody>
       </table>
+      {noOptionsMessage}
     </>
   );
 };
+
 
 export default BasicSelect;
