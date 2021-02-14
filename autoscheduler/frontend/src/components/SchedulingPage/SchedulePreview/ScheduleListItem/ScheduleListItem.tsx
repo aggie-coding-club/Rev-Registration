a05346dc757e29468671cf22c71103217a4f7389
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-02-14 12:54:05 -0600

    Reversed section order in ScheduleListItem

--- autoscheduler/frontend/src/components/SchedulingPage/SchedulePreview/ScheduleListItem/ScheduleListItem.tsx
+++ autoscheduler/frontend/src/components/SchedulingPage/SchedulePreview/ScheduleListItem/ScheduleListItem.tsx
@@ -84,7 +84,8 @@ const ScheduleListItem: React.FC<ScheduleListItemProps> = ({ index }) => {
               {`${sec.subject} ${sec.courseNum}-${sec.sectionNum}`}
               <br />
             </span>
-          ))
+          // Reverse the direction that the sections are displayed in to match to the CourseCards
+          )).reverse()
         }
       />
       <MiniSchedule schedule={schedule.meetings} />
