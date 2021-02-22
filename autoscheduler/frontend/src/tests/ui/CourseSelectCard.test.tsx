7cbbf2a66e331f50a8b319ad9f7226214d913aca
Adel Hassan <sean.dashals@gmail.com>
2021-02-21 20:43:44 -0600

    Added test for remote

--- autoscheduler/frontend/src/tests/ui/CourseSelectCard.test.tsx
+++ autoscheduler/frontend/src/tests/ui/CourseSelectCard.test.tsx
@@ -524,6 +524,32 @@ describe('Course Select Card UI', () => {
 
       expect(await findByLabelText('No Meeting Times:')).toHaveTextContent('No Preference');
     });
+
+    test('is set to No Preference for Remote', async () => {
+      // Arrange
+      // sessions/get_saved_courses
+      fetchMock.mockResponseOnce(JSON.stringify({ // api/course/search
+        results: ['PSYC 107'], // PSYC has a remote section in testFetch
+      }));
+      fetchMock.mockImplementationOnce(testFetch); // api/sections
+
+      const store = createStore(autoSchedulerReducer, applyMiddleware(thunk));
+      store.dispatch(setTerm('202031'));
+
+      const { getByLabelText, findByLabelText, findByText } = render(
+        <Provider store={store}>
+          <CourseSelectCard id={0} />
+        </Provider>,
+      );
+
+      // fill in course
+      const courseEntry = getByLabelText('Course') as HTMLInputElement;
+      fireEvent.click(courseEntry);
+      fireEvent.change(courseEntry, { target: { value: 'P' } });
+      fireEvent.click(await findByText('PSYC 107'));
+
+      expect(await findByLabelText('Remote:')).toHaveTextContent('No Preference');
+    });
   });
 
   describe('shows the appropriate instructional method for sections', () => {
