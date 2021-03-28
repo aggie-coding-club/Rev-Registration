5361d59fb64823f7f30ad6366d64d3436c640775
Ryan Conn <rconn478@gmail.com>
2021-03-28 14:26:45 -0500

    Use ADMINS to send mail

--- autoscheduler/autoscheduler/settings/base.py
+++ autoscheduler/autoscheduler/settings/base.py
@@ -14,6 +14,9 @@ _IS_PROXY = os.getenv('SETTINGS_MODE') == 'proxy'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
 if IS_GCP:
     # For production, load the secret from secret.txt and set it
@@ -22,17 +25,15 @@ if IS_GCP:
 
     with open(_SECRET_PATH) as _SECRET_FILE:
         SECRET_KEY = _SECRET_FILE.read()[:-1] # Shave off newline
-
-    # Set up google cloud logging
-    import google.cloud.logging
-    CLOUD_LOGGING_CLIENT = google.cloud.logging.Client()
-    CLOUD_LOGGING_CLIENT.get_default_handler()
-    CLOUD_LOGGING_CLIENT.setup_logging()
 else:
     SECRET_KEY = 'ogxjva5h%&5c7&e#-2f1&+u5p#zygwffcy!@)k8i37#j_89xe2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not IS_GCP
+
+# When an error occurs with DEBUG=False, Django will send emails to people listed below
+# with stack trace and other information
+ADMINS = [('Ryan Conn', 'rconn478@gmail.com')]
 
 ALLOWED_HOSTS = [
     '*',
