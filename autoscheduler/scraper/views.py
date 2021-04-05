3928c1777c683988e21bce9b49b028f43f3f856e
Ryan Conn <rconn478@gmail.com>
2021-04-05 16:52:30 -0500

    Revert "[REVERT] Test logging error with and without import"
    This reverts commit de636c7fdcc17d7667d38c3fa96cdda4ee8c8564.

--- autoscheduler/scraper/views.py
+++ autoscheduler/scraper/views.py
@@ -1,5 +1,4 @@
 from itertools import chain, islice
-import logging
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -29,9 +28,6 @@ class ListSectionView(generics.ListAPIView):
 
     def get_queryset(self):
         """ Overrides default behavior of get_queryset() to work without a primary key """
-        # Temp: test that logging works without cloud logging import
-        logging.error('(Test, not an actual error): logging.error shows without import')
-
         dept = self.request.query_params.get('dept')
         course_num = self.request.query_params.get('course_num')
         term = self.request.query_params.get('term')
