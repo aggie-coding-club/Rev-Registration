b8d73ddba2a277f4811e4092e17236647b12a3a9
ThatJuanGuy <ThatJuanGuy@users.noreply.github.com>
2021-04-18 16:48:48 -0500

    Added HowToUse Page link to Landing Page

--- autoscheduler/frontend/src/components/LandingPage/LandingPage.tsx
+++ autoscheduler/frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { Typography } from '@material-ui/core';
 import HelpText from './HelpText/HelpText';
 import SelectTerm from './SelectTerm/SelectTerm';
 import * as styles from './LandingPage.css';
@@ -12,6 +13,9 @@ const LandingPage: React.FC<RouteComponentProps> = () => (
     <SelectTerm />
     <div className={styles.dialogContainer}>
       <div className={styles.dialogLink}>
+        <Typography>
+          <a href="/info" className={styles.aLink}>How To Use</a>
+        </Typography>
         <About />
         <PrivacyPolicy />
       </div>
