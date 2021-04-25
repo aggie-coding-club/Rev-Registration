1594cd07fb406bcef87fc839abcbe59785fc3d9f
Gannon Prudhomme <gannonprudhomme@users.noreply.github.com>
2021-04-25 13:28:59 -0500

    Set term to null on landing page (#539)
    * Re-added set term to null on landing page
    
    * cleanup

--- autoscheduler/frontend/src/components/LandingPage/LandingPage.tsx
+++ autoscheduler/frontend/src/components/LandingPage/LandingPage.tsx
@@ -1,22 +1,31 @@
 import * as React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { useDispatch } from 'react-redux';
 import HelpText from './HelpText/HelpText';
 import SelectTerm from './SelectTerm/SelectTerm';
 import * as styles from './LandingPage.css';
 import About from './About/About';
 import PrivacyPolicy from './PrivacyPolicy/PrivacyPolicy';
+import setTerm from '../../redux/actions/term';
 
-const LandingPage: React.FC<RouteComponentProps> = () => (
-  <div className={styles.container}>
-    <HelpText />
-    <SelectTerm />
-    <div className={styles.dialogContainer}>
-      <div className={styles.dialogLink}>
-        <About />
-        <PrivacyPolicy />
+const LandingPage: React.FC<RouteComponentProps> = () => {
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(setTerm(null));
+  }, [dispatch]);
+
+  return (
+    <div className={styles.container}>
+      <HelpText />
+      <SelectTerm />
+      <div className={styles.dialogContainer}>
+        <div className={styles.dialogLink}>
+          <About />
+          <PrivacyPolicy />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default LandingPage;
