e20ab9d1d96dc7d611e8dc2d93547d30dc2d6dad
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-05-09 14:38:53 -0500

    Retrieved & displayed room on frontend

--- autoscheduler/frontend/src/utils/meetingBuilding.ts
+++ autoscheduler/frontend/src/utils/meetingBuilding.ts
@@ -1,5 +1,5 @@
 import Meeting from '../types/Meeting';
 
 export default function meetingBuilding(meeting: Meeting): string {
-  return meeting.building || 'ONLINE';
+  return meeting.building ? `${meeting.building} ${meeting.room}` : 'ONLINE';
 }
