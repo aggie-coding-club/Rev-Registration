82fc6347426342641dab42c81fd49bc00889eb68
Troy Lee <troylee3006@gmail.com>
2021-06-06 12:03:51 -0500

    begin fixing scrolling issue

--- autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/SectionSelect/SectionSelect.css.d.ts
+++ autoscheduler/frontend/src/components/SchedulingPage/CourseSelectColumn/CourseSelectCard/ExpandedCourseCard/SectionSelect/SectionSelect.css.d.ts
@@ -1,5 +1,6 @@
 declare namespace SectionSelectCssNamespace {
   export interface ISectionSelectCss {
+    "MuiCollapse-container": string;
     "MuiExpansionPanelSummary-content": string;
     "accordian-details": string;
     "accordian-root": string;
@@ -37,6 +38,7 @@ declare namespace SectionSelectCssNamespace {
     meetingInfoWrapper: string;
     meetingTime: string;
     meetingType: string;
+    muiCollapseContainer: string;
     muiExpansionPanelSummaryContent: string;
     "my-icon-button": string;
     "my-list-item-icon": string;
