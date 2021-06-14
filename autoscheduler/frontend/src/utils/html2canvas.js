15b48f8248823a7ab8bed777b3887f55b25b689e
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-06-14 15:29:33 -0500

    Added html2canvas js file

---
+++ autoscheduler/frontend/src/utils/html2canvas.js
@@ -0,0 +1,11 @@
+import * as html2canvas from 'html2canvas'; // This import is incorrect, so we made this a JS file
+import saveImage from './saveImage';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function execute2canvas(options, scheduleRef) {
+  html2canvas(scheduleRef.current, options).then(
+    (canvas) => {
+      saveImage(canvas.toDataURL(), 'image.png');
+    },
+  );
+}
