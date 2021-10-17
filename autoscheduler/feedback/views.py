42e69c5cdf96e4d52692f50607ec539676978c3c
Ryan Conn <rconn478@gmail.com>
2021-10-17 10:53:50 -0500

    Send discord message on feedback submission

--- autoscheduler/feedback/views.py
+++ autoscheduler/feedback/views.py
@@ -1,7 +1,11 @@
+import os
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from feedback.models import FeedbackFormResponse
+from discord_bot import send_discord_message
+
+DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_FEEDBACK_CHANNEL_ID') or -1)
 
 @api_view(['POST'])
 @parser_classes([JSONParser])
@@ -15,4 +19,5 @@ def submit_feedback(request):
         return Response(status=400)
 
     FeedbackFormResponse(rating=rating, comment=comment).save()
+    send_discord_message(DISCORD_CHANNEL_ID, f'New rating ({rating}/5):\n\n{comment}')
     return Response()
