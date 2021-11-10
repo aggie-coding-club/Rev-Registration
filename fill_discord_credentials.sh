47c9d4ef1386fd32a13b9d7ba0befeac031377d2
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-11-09 22:58:19 -0800

    Added discord filling to deploy + updated script

--- fill_discord_credentials.sh
+++ fill_discord_credentials.sh
@@ -1,2 +1,2 @@
 # -e enables interpretation of backslash escapes (so we can use \n for a new line)
-echo -e "DISCORD_BOT_TOKEN='$1'\nDISCORD_SCRAPE_CHANNEL_ID='$2'" > autoscheduler/autoscheduler/settings/.env
+echo -e "DISCORD_BOT_TOKEN='$1'\nDISCORD_$2_CHANNEL_ID='$3'" > autoscheduler/autoscheduler/settings/.env
