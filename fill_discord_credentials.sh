7f76d5f5f54c6880a82f3351befbf4cacc3237af
Gannon Prudhomme <gannonprudhomme@users.noreply.github.com>
2021-11-10 01:46:51 -0600

    Fix feedback form deploy by filling the discord credentials on deploy workflow (#620)

--- fill_discord_credentials.sh
+++ fill_discord_credentials.sh
@@ -1,2 +1,3 @@
 # -e enables interpretation of backslash escapes (so we can use \n for a new line)
-echo -e "DISCORD_BOT_TOKEN='$1'\nDISCORD_SCRAPE_CHANNEL_ID='$2'\nDISCORD_FEEDBACK_CHANNEL_ID='$3'" > autoscheduler/autoscheduler/settings/.env
+# Note that >> appends to a file, whereas just > will overwrite the file
+echo -e "DISCORD_BOT_TOKEN='$1'\nDISCORD_SCRAPE_CHANNEL_ID='$2'\nDISCORD_FEEDBACK_CHANNEL_ID='$3'" >> autoscheduler/autoscheduler/settings/.env
--- fill_google_auth_credentials.sh
+++ fill_google_auth_credentials.sh
@@ -1,2 +1,3 @@
 # -e enables interpretation of backslash escapes (so we can use \n for a new line)
-echo -e "GOOGLE_OAUTH2_CLIENT_ID='$1'\nGOOGLE_OAUTH2_SECRET='$2'" > autoscheduler/autoscheduler/settings/.env
+# Note that >> appends to a file, whereas just > will overwrite the file
+echo -e "GOOGLE_OAUTH2_CLIENT_ID='$1'\nGOOGLE_OAUTH2_SECRET='$2'" >> autoscheduler/autoscheduler/settings/.env
