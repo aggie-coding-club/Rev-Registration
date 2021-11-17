aaf4ae7edbc081d0f3b11102ed18fb2e6aa4213b
Gannon Prudhomme <gannonprudhomme@gmail.com>
2021-11-16 18:13:23 -0800

    Corrected tests

--- autoscheduler/feedback/views.py
+++ autoscheduler/feedback/views.py
@@ -4,6 +4,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from feedback.models import FeedbackFormResponse
 from discord_bot import send_discord_message_webhook
+from autoscheduler.settings.base import IS_GCP
 
 DISCORD_FEEDBACK_WEBHOOK_URL = os.getenv('DISCORD_FEEDBACK_WEBHOOK_URL')
 
@@ -19,6 +20,7 @@ def submit_feedback(request):
         return Response(status=400)
 
     FeedbackFormResponse(rating=rating, comment=comment).save()
-    send_discord_message_webhook(DISCORD_FEEDBACK_WEBHOOK_URL, f'New rating ({rating}/5):\n{comment}')
+    if IS_GCP:
+        send_discord_message_webhook(DISCORD_FEEDBACK_WEBHOOK_URL, f'New rating ({rating}/5):\n{comment}')
 
     return Response()
